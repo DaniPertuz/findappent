2c90dd5faee6f0e2e343bdb0c16600b8d02fabb2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 20:59:05 -0500

    Callout UI component and styles

---
+++ src/presentation/components/ui/Callout/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  bodySmall: {
+    fontFamily: 'Inter_18pt-Regular',
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: -0.28,
+    lineHeight: 20,
+  },
+});
