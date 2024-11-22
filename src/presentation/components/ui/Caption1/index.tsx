de553b865a4006a778cabacb0bb97823f47bc6e9
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 21:06:59 -0500

    Caption 1 UI component and styles

---
+++ src/presentation/components/ui/Caption1/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { Text } from 'react-native';
+import { styles } from './styles';
+import { useColors } from '../../../theme/colors';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const Caption1: FC<Props> = ({ children }) => {
+  const { mainText } = useColors();
+  return (
+    <Text style={{ ...styles.caption, color: mainText }}>{children}</Text>
+  );
+};
