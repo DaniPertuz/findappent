e788a820cb560017acfc30046d48ede2c3585f8f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 21:09:14 -0500

    Caption 2 UI component and styles

---
+++ src/presentation/components/ui/Caption2/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  caption: {
+    fontFamily: 'Inter_18pt-Medium',
+    fontSize: 12,
+    fontWeight: '500',
+    letterSpacing: -0.28,
+    lineHeight: 20,
+  },
+});
