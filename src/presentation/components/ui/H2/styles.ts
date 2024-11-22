334cc6ba9c89a33d8e0e6e7371aa1e4d9be7ae9b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 20:36:39 -0500

    H2 UI component and styles

---
+++ src/presentation/components/ui/H2/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  h2: {
+    fontFamily: 'Inter_28pt-Bold',
+    fontSize: 28,
+    fontWeight: '700',
+    letterSpacing: -0.28,
+    lineHeight: 32,
+  },
+});
