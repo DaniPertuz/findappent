713d5860927317bc102be23deedbbe8fce2f2855
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 21:01:41 -0500

    Subheadline UI component and styles

---
+++ src/presentation/components/ui/Subheadline/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  subheadline: {
+    fontFamily: 'Inter_18pt-Bold',
+    fontSize: 14,
+    fontWeight: '700',
+    letterSpacing: -0.28,
+    lineHeight: 18,
+  },
+});
