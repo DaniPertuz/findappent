852334f2a788e947ef62ee92a15ab46ab56f92e9
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 20:10:07 -0500

    Custom useColors hook for app colors handling

---
+++ src/presentation/theme/colors.ts
@@ -0,0 +1,20 @@
+import { useColorScheme } from 'react-native';
+
+export const useColors = () => {
+  const colorScheme = useColorScheme();
+
+  return {
+    darkBlue: '#207CFD',
+    lightBlue: '#58D7FE',
+    black: '#2F2F2F',
+    white: colorScheme === 'dark' ? '#081023' : '#FAFAFA',
+    mainText: colorScheme === 'dark' ? '#FAFAFA' : '#081023',
+    primaryText: '#1F273A',
+    secondaryText: '#5A5A5A',
+    tertiaryText: '#858585',
+    quaternaryText: '#DEDEDE',
+    error: '#D13232',
+    success: '#26DFB3',
+    alert: '#F9C749',
+  };
+};
