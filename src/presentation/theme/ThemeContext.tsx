ce7234feb3fa3ec1e19706d3b89e8e21dc3cb326
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-21 20:10:41 -0500

    Implement useColors on themes

--- src/presentation/theme/ThemeContext.tsx
+++ src/presentation/theme/ThemeContext.tsx
@@ -2,62 +2,42 @@ import React, { createContext, PropsWithChildren, useEffect, useState } from 're
 import { useColorScheme } from 'react-native';
 import { NavigationContainer, DarkTheme as NavigationDarkTheme, DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
 import { adaptNavigationTheme, PaperProvider } from 'react-native-paper';
+import { useColors } from './colors';
 
 const { LightTheme: NavLightTheme, DarkTheme: NavDarkTheme } = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
   reactNavigationDark: NavigationDarkTheme,
 });
 
-const CustomLightTheme = {
-  ...NavLightTheme,
-  colors: {
-    ...NavLightTheme.colors,
-    darkBlue: '#207CFD',
-    lightBlue: '#58D7FE',
-    black: '#2F2F2F',
-    white: '#FAFAFA',
-    mainText: '#081023',
-    primaryText: '#1F273A',
-    secondaryText: '#5A5A5A',
-    tertiaryText: '#858585',
-    quaternaryText: '#DEDEDE',
-    error: '#D13232',
-    success: '#26DFB3',
-    alert: '#F9C749',
-  },
-};
-
-const CustomDarkTheme = {
-  ...NavDarkTheme,
-  colors: {
-    ...NavDarkTheme.colors,
-    darkBlue: '#207CFD',
-    lightBlue: '#58D7FE',
-    black: '#2F2F2F',
-    white: '#081023',
-    mainText: '#FAFAFA',
-    primaryText: '#1F273A',
-    secondaryText: '#5A5A5A',
-    tertiaryText: '#858585',
-    quaternaryText: '#DEDEDE',
-    error: '#D13232',
-    success: '#26DFB3',
-    alert: '#F9C749',
-  },
-};
-
 export const ThemeContext = createContext({
   isDark: false,
   theme: NavLightTheme,
-  setDarkTheme: () => {},
-  setLightTheme: () => {},
-  setSystemTheme: () => {},
+  setDarkTheme: () => { },
+  setLightTheme: () => { },
+  setSystemTheme: () => { },
 });
 
 export const ThemeContextProvider = ({ children }: PropsWithChildren) => {
   const colorScheme = useColorScheme();
   const [isDark, setIsDark] = useState(colorScheme === 'dark');
   const [useSystemTheme, setUseSystemTheme] = useState(true);
+  const baseColors = useColors();
+
+  const CustomLightTheme = {
+    ...NavLightTheme,
+    colors: {
+      ...NavLightTheme.colors,
+      ...baseColors,
+    },
+  };
+
+  const CustomDarkTheme = {
+    ...NavDarkTheme,
+    colors: {
+      ...NavDarkTheme.colors,
+      ...baseColors,
+    },
+  };
   const theme = isDark ? CustomDarkTheme : CustomLightTheme;
 
   const setLightTheme = () => {
