b8e73f72320b60191896d35e88dae69c7235b750
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-23 12:05:32 -0500

    Set dynamic fill color

--- src/presentation/hooks/useIcons.tsx
+++ src/presentation/hooks/useIcons.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import SvgUri from 'react-native-svg-uri';
 import { icons } from '../../utils/icons';
-import { useColors } from '../theme/colors';
 
-export const useIcons = (name: string, height: number, width: number) => {
+export const useIcons = (name: string, height: number, width: number, fill: string) => {
   const iconUri = icons[name];
-  const { mainText } = useColors();
 
   if (!iconUri) {
     return null;
   }
 
-  return <SvgUri width={width} height={height} source={iconUri} fill={mainText} />;
+  return <SvgUri width={width} height={height} source={iconUri} fill={fill} />;
 };
