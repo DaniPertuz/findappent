4884883cbe10a7c675f588f284c524f37551fa45
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-22 23:01:48 -0500

    Packages and config to display icons and images

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      // assetExts: assetExts.filter(ext => ext !== 'svg'),
+      // sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx', 'svg'],
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
