867129a52df8d0e18e9f47a5809551e50e809edf
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-22 23:04:31 -0500

    Hide warnings in dev environment

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import FindAppEnt from './FindAppEnt';
 import {name as appName} from './app.json';
 
+if (__DEV__) {
+  console.warn = () => {};
+}
+
 AppRegistry.registerComponent(appName, () => FindAppEnt);
