6f49fcd6512733b5bfc2721768323a88b7746d87
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-24 22:48:42 -0500

    ThemeColors interface

---
+++ src/presentation/theme/ThemeColors.tsx
@@ -0,0 +1,17 @@
+export interface ThemeColors {
+  darkBlue: string;
+  lightBlue: string;
+  black: string;
+  brandMainText: string;
+  brandWhite: string;
+  white: string;
+  mainText: string;
+  primaryText: string;
+  secondaryText: string;
+  tertiaryText: string;
+  quaternaryText: string;
+  error: string;
+  success: string;
+  alert: string;
+  gray: string;
+}
