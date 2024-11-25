25e259efc3670bb694a769de0cd4f616a1211fda
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-24 20:43:11 -0500

    Update metro config for SVG files handling

--- metro.config.js
+++ metro.config.js
@@ -2,15 +2,14 @@ const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: { sourceExts },
+    resolver: { assetExts, sourceExts },
   } = await getDefaultConfig();
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
-      // assetExts: assetExts.filter(ext => ext !== 'svg'),
-      // sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx', 'svg'],
+      assetExts: [...assetExts.filter(ext => ext !== 'svg'), 'png'],
       sourceExts: [...sourceExts, 'svg'],
     },
   };
