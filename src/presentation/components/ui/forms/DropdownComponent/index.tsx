c4d573595112b79cfa30e05e7abb23288b2edf46
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-24 20:42:19 -0500

    UI Dropdown component and styles

---
+++ src/presentation/components/ui/forms/DropdownComponent/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC, useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { styles } from './styles';
+
+interface Props {
+  items: { label: string; value: string }[];
+}
+
+export const DropdownComponent: FC<Props> = ({ items }) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [list, setList] = useState(
+    items.map((item) => ({
+      label: `${item.label}`,
+      value: item.value,
+    }))
+  );
+
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={list}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setList}
+      placeholder={'Categoría'}
+      style={styles.dropdown}
+    />
+  );
+};
---
+++ src/presentation/components/ui/forms/DropdownComponent/styles.ts
@@ -0,0 +1,5 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  dropdown: { alignSelf: 'center', height: 60, width: '90%' },
+});
