d74c575a1edef8b3eb6194d2d50561bca2ffa564
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-29 23:39:23 -0500

    UI StatusBarComponent custom component

---
+++ src/presentation/components/ui/StatusBarComponent/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Platform, StatusBar, View } from 'react-native';
+
+interface Props {
+  color: string;
+  theme: 'dark-content' | 'light-content';
+}
+
+const StatusBarComponent = ({ color, theme }: Props) => {
+
+  const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+  return (
+    <View style={{ height: STATUS_BAR_HEIGHT, backgroundColor: color }}>
+      <StatusBar translucent backgroundColor={color} barStyle={theme} />
+    </View>
+  );
+};
+
+export default StatusBarComponent;
