a614d757c1f0f035d8f51476fe1749ca54b1ae6e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-01 21:34:33 -0500

    Custom AuthFormContainer component to contain auth forms

---
+++ src/presentation/components/auth/AuthFormContainer/index.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useContext } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { ThemeContext } from '../../../theme/ThemeContext';
+
+const { width, height } = Dimensions.get('window');
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AuthFormContainer: FC<Props> = ({ children }) => {
+  const { colors, currentTheme } = useContext(ThemeContext);
+  const borders = {
+    backgroundColor: colors.white,
+    borderTopColor: currentTheme === 'light' ? undefined : colors.brandWhite,
+    borderBottomColor: currentTheme === 'light' ? undefined : colors.brandWhite,
+    borderEndColor: currentTheme === 'light' ? undefined : colors.brandWhite,
+    borderStartColor: currentTheme === 'light' ? undefined : colors.brandWhite,
+    borderWidth: currentTheme === 'light' ? 0 : 1,
+  };
+
+  return (
+    <View style={[styles.loginFormContainer, borders]}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loginFormContainer: {
+    borderRadius: 16,
+    height: height * 0.85,
+    marginVertical: 45,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    width: width * 0.9,
+  },
+});
+
+export default AuthFormContainer;
