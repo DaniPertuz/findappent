d8100d1655094f46462883c61b621785e1409527
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-01 21:10:58 -0500

    Update component props

--- src/presentation/components/ui/forms/DefaultInput/index.tsx
+++ src/presentation/components/ui/forms/DefaultInput/index.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useContext } from 'react';
 import { KeyboardTypeOptions, TextInput, View } from 'react-native';
-import { useIcons } from '../../../../hooks/useIcons';
 import { useColors } from '../../../../theme/colors';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 import { useStyles } from '../../forms/styles';
@@ -9,18 +8,16 @@ interface Props {
   field: string;
   fieldValue: 'name' | 'category' | 'other' | 'phone' | 'whatsapp' | 'instagram';
   placeholder: string;
-  icon: string;
+  iconElement: React.JSX.Element
   keyboardType: KeyboardTypeOptions;
   warning?: boolean;
   onChange: (value: string, field: 'name' | 'category' | 'other' | 'phone' | 'whatsapp' | 'instagram') => void;
 }
 
-const DefaultInput: FC<Props> = ({ field, fieldValue, placeholder, icon, keyboardType, warning, onChange }) => {
+const DefaultInput: FC<Props> = ({ field, fieldValue, placeholder, iconElement, keyboardType, warning, onChange }) => {
   const { currentTheme } = useContext(ThemeContext);
   const { brandMainText, gray } = useColors(currentTheme === 'light');
   const { input, inputContainer, warningBorder } = useStyles();
-
-  const iconElement = useIcons(icon, 20, 20, brandMainText);
   const containerStyle = [
     inputContainer,
     warning && warningBorder,
