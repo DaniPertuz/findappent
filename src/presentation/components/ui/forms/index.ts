0839a501e9e3f44d663bad437e951b78697339d4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-05 23:40:41 -0500

    Set index file

---
+++ src/presentation/components/ui/forms/index.ts
@@ -0,0 +1,5 @@
+export { default as AddressInput } from './AddressInput';
+export { default as DefaultInput } from './DefaultInput';
+export { default as DescriptionInput } from './DescriptionInput';
+export { default as DropdownComponent } from './DropdownComponent';
+export { default as PasswordInput } from './PasswordInput';
