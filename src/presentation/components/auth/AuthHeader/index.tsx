5baed9f53236341733d6800c1df3db87a3a5cb78
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-07 00:11:23 -0500

    Custom AuthHeader auth component and styles

---
+++ src/presentation/components/auth/AuthHeader/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { BodySmall } from '../../ui/BodySmall';
+import { H4 } from '../../ui/H4';
+import { ThemeContext } from '../../../theme/ThemeContext';
+
+interface Props {
+  title: string;
+  subtitle: string;
+}
+
+const AuthHeader: FC<Props> = ({ title, subtitle }) => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <View style={styles.labelsContainer}>
+      <H4 customColor={colors.mainText}>{title}</H4>
+      <BodySmall customColor={colors.mainText}>{subtitle}</BodySmall>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  labelsContainer: {
+    alignSelf: 'stretch',
+    gap: 5,
+  },
+});
+
+export default AuthHeader;
