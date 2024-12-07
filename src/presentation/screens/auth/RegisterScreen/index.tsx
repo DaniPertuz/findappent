7421d2b8106dd3a7ee09ac2709334da71cb7df36
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-06 22:38:21 -0500

    RegisterScreen screen and styles

---
+++ src/presentation/screens/auth/RegisterScreen/index.tsx
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from 'react';
+import { View } from 'react-native';
+import { MainView } from '../../../components/ui/MainView';
+import StatusBarComponent from '../../../components/ui/StatusBarComponent';
+import { ThemeContext } from '../../../theme/ThemeContext';
+import AuthButton from '../../../components/auth/AuthButton';
+import AuthFooter from '../../../components/auth/AuthFooter';
+import AuthFormContainer from '../../../components/auth/AuthFormContainer';
+import RenderInputWithWarning from '../../../components/auth/RenderInputWithWarning';
+import { Background } from '../../../components/ui/Background';
+import { BodySmall } from '../../../components/ui/BodySmall';
+import { H4 } from '../../../components/ui/H4';
+import { styles } from './styles';
+
+const RegisterScreen = () => {
+  const { colors, currentTheme } = useContext(ThemeContext);
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  return (
+    <MainView>
+      <Background />
+      <StatusBarComponent color={colors.white} theme={currentTheme === 'light' ? 'dark-content' : 'light-content'} />
+      <AuthFormContainer>
+        <View style={styles.container}>
+          <H4 customColor={colors.mainText}>Crea tu cuenta</H4>
+          <BodySmall customColor={colors.mainText}>Ingresa tus datos para crear una cuenta</BodySmall>
+          <View style={styles.inputsContainer}>
+            <RenderInputWithWarning label={'Nombre de la empresa'} placeholder={'Ingresa tu nombre'} customMessage={'Ingresa el nombre de la empresa'} value={name} onChange={setName} />
+            <RenderInputWithWarning label={'Correo corporativo'} placeholder={'Ingresa tu correo'} isEmail value={email} onChange={setEmail} />
+            <RenderInputWithWarning label={'Contraseña'} placeholder={'Ingresa tu contraseña'} isPassword value={password} onChange={setPassword} />
+            <AuthButton text={'Crear cuenta'} />
+          </View>
+        </View>
+        <AuthFooter label={'¿Ya tienes una empresa registrada'} link={'Inicia sesión'} />
+      </AuthFormContainer>
+    </MainView>
+  );
+};
+
+export default RegisterScreen;
---
+++ src/presentation/screens/auth/RegisterScreen/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+    paddingHorizontal: 10,
+    gap: 5,
+  },
+  inputsContainer: {
+    gap: 15,
+    marginTop: 10,
+  },
+});
