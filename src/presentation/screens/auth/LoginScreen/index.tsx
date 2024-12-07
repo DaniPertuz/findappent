b43f3675b0596675f8c2eaa1edda803aa8b28db7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-06 22:40:49 -0500

    Implement custom auth components

--- src/presentation/screens/auth/LoginScreen/index.tsx
+++ src/presentation/screens/auth/LoginScreen/index.tsx
@@ -1,12 +1,11 @@
 import React, { useContext, useState } from 'react';
 import { View, Image, Pressable, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import AuthButton from '../../../components/auth/AuthButton';
+import AuthFooter from '../../../components/auth/AuthFooter';
 import AuthFormContainer from '../../../components/auth/AuthFormContainer';
 import RenderInputWithWarning from '../../../components/auth/RenderInputWithWarning';
 import { Background } from '../../../components/ui/Background';
-import { Body } from '../../../components/ui/Body';
 import { BodySmall } from '../../../components/ui/BodySmall';
-import { ButtonComponent } from '../../../components/ui/ButtonComponent';
-import { Caption1 } from '../../../components/ui/Caption1';
 import { Caption2 } from '../../../components/ui/Caption2';
 import { Footnote } from '../../../components/ui/Footnote';
 import { H4 } from '../../../components/ui/H4';
@@ -41,22 +40,15 @@ const LoginScreen = () => {
                 <H4 customColor={colors.mainText}>Bienvenido</H4>
                 <BodySmall customColor={colors.mainText}>Ingresa tus credenciales para continuar</BodySmall>
               </View>
-              <RenderInputWithWarning label={'Correo corporativo'} value={email} placeholder={'Ingresa tu correo'} onChange={setEmail} />
+              <RenderInputWithWarning label={'Correo corporativo'} value={email} placeholder={'Ingresa tu correo'} onChange={setEmail} isEmail={true} />
               <RenderInputWithWarning label={'Contraseña'} value={password} placeholder={'Ingresa tu contraseña'} onChange={setPassword} />
               <View style={styles.forgotPasswordContainer}>
                 <Pressable>
                   <Caption2 customColor={colors.darkBlue}>¿Olvidaste tu contraseña?</Caption2>
                 </Pressable>
               </View>
-              <View style={styles.loginButtonContainer}>
-                <ButtonComponent>
-                  <Body customColor={colors.brandWhite}>Iniciar Sesión</Body>
-                </ButtonComponent>
-              </View>
-              <View style={styles.createAccountContainer}>
-                <Caption1 customColor={colors.mainText}>¿No tienes una empresa registrada?</Caption1>
-                <Caption1 customColor={colors.darkBlue}>Crear cuenta</Caption1>
-              </View>
+              <AuthButton text={'Iniciar sesión'} />
+              <AuthFooter label={'¿No tienes una empresa registrada?'} link={'Crear cuenta'} />
             </View>
           </AuthFormContainer>
         </ScrollView>
