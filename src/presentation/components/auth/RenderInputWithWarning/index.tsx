fc09be412b0074c5875edf6572f35eb53a32b217
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-06 22:33:25 -0500

    Add custom styles and property for conditional rendering

--- src/presentation/components/auth/RenderInputWithWarning/index.tsx
+++ src/presentation/components/auth/RenderInputWithWarning/index.tsx
@@ -2,6 +2,7 @@ import React, { FC, useContext } from 'react';
 import { View, Image } from 'react-native';
 import { Caption2 } from '../../ui/Caption2';
 import { PasswordInput, DefaultInput } from '../../ui/forms';
+import { appStyles } from '../../../theme/app-styles';
 import { ThemeContext } from '../../../theme/ThemeContext';
 import { styles } from './styles';
 
@@ -10,11 +11,12 @@ interface Props {
   value: string;
   placeholder: string;
   customMessage?: string;
+  isEmail?: boolean;
   isPassword?: boolean;
   onChange: (value: string) => void;
 }
 
-const RenderInputWithWarning: FC<Props> = ({ label, value, placeholder, customMessage, isPassword = false, onChange }) => {
+const RenderInputWithWarning: FC<Props> = ({ label, value, placeholder, customMessage, isEmail = false, isPassword = false, onChange }) => {
   const { colors } = useContext(ThemeContext);
   return (
     <View style={styles.inputsContainer}>
@@ -25,15 +27,24 @@ const RenderInputWithWarning: FC<Props> = ({ label, value, placeholder, customMe
         ) : (
           <DefaultInput
             field={value}
-            fieldValue={'email'}
+            fieldValue={isEmail ? 'email' : 'name'}
             placeholder={placeholder}
-            iconElement={<Image source={require('../../../../assets/icons/envelope.png')} style={styles.inputIcon} />}
-            keyboardType="default"
+            iconElement={
+              <Image
+                source={
+                  isEmail
+                    ? require('../../../../assets/icons/envelope.png')
+                    : require('../../../../assets/icons/users.png')
+                }
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={isEmail ? 'email-address' : 'default'}
             onChange={onChange} />
         )}
         {value === '' && (
           <View style={styles.warningContainer}>
-            <Image style={styles.inputIcon} source={require('../../../../assets/icons/warning.png')} />
+            <Image style={appStyles.inputIcon} source={require('../../../../assets/icons/warning.png')} />
             <Caption2 customColor={colors.error}>{customMessage ?? `Ingresa tu ${label.toLowerCase()}`}</Caption2>
           </View>
         )}
