506b89c976aa79e5f71539bfafe3691ef7fe1969
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-08 21:51:44 -0500

    DashboardHeader Dashboard component and styles

---
+++ src/presentation/components/dashboard/DashboardHeader/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useContext } from 'react';
+import { Image, View } from 'react-native';
+import { CLOUDINARY_URL } from '../../../../api/cloudinaryIconsAPI';
+import { BodySmall } from '../../ui/BodySmall';
+import { Footnote } from '../../ui/Footnote';
+import { Subheadline } from '../../ui/Subheadline';
+import { ThemeContext } from '../../../theme/ThemeContext';
+import { styles } from './styles';
+
+interface Props {
+  title: string;
+  subtitle: string;
+}
+
+const DashboardHeader: FC<Props> = ({ title, subtitle }) => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Subheadline customColor={colors.mainText}>{title}</Subheadline>
+        <BodySmall customColor={colors.mainText}>{subtitle}</BodySmall>
+        <Footnote customColor={colors.mainText}>Nivel</Footnote>
+      </View>
+      <Image source={{ uri: `${CLOUDINARY_URL}/fa_blue` }} style={styles.mainPhoto} />
+    </View>
+  );
+};
+
+export default DashboardHeader;
---
+++ src/presentation/components/dashboard/DashboardHeader/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    gap: 5,
+    marginTop: 40,
+  },
+  headerContainer: {
+    flex: 1,
+    gap: 5,
+  },
+  mainPhoto: {
+    alignSelf: 'center',
+    borderRadius: 32,
+    height: 62,
+    width: 62,
+  },
+});
