196f537a5dc496f3e277700fd42d7959b3f49d74
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-09 22:51:25 -0500

    ProfileDetail profile component and styles

---
+++ src/presentation/components/profile/ProfileDetail/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useContext } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { BodySmall } from '../../ui/BodySmall';
+import { Caption2 } from '../../ui/Caption2';
+import ProfileLabelContainer from '../ProfileLabelContainer';
+import { ThemeContext } from '../../../theme/ThemeContext';
+
+interface Props {
+  label: string;
+  icon: string;
+  text: string;
+}
+
+const ProfileDetail: FC<Props> = ({ label, icon, text }) => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <ProfileLabelContainer>
+      <Caption2 customColor={colors.gray}>{label}</Caption2>
+      <View style={styles.container}>
+        <Image source={{ uri: icon }} style={styles.detailIcon} />
+        <BodySmall customColor={colors.mainText}>{text}</BodySmall>
+      </View>
+    </ProfileLabelContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { alignItems: 'center', flexDirection: 'row', gap: 10 },
+  detailIcon: { height: 25, width: 25 },
+});
+
+export default ProfileDetail;
