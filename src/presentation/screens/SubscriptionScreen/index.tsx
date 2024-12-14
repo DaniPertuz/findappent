5401961e6c40e804a8dedc38cbc371217be6d3e0
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-14 15:47:52 -0500

    SubscriptionScreen screen and styles

---
+++ src/presentation/screens/SubscriptionScreen/index.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import SubscriptionHeader from '../../components/subscriptions/SubscriptionHeader';
+import SubscriptionPlansList from '../../components/subscriptions/SubscriptionPlansList';
+import StatusBarComponent from '../../components/ui/StatusBarComponent';
+import { ThemeContext } from '../../theme/ThemeContext';
+
+const SubscriptionScreen = () => {
+  const { colors, currentTheme } = useContext(ThemeContext);
+  return (
+    <View style={[styles.container, { backgroundColor: colors.white }]}>
+      <StatusBarComponent color={colors.white} theme={currentTheme === 'light' ? 'dark-content' : 'light-content'} />
+      <SubscriptionHeader />
+      <SubscriptionPlansList />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
+
+export default SubscriptionScreen;
