8d55a20f79e7c899b0a14218021083b0389fd0fb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-14 15:50:34 -0500

    Implementing getIconUrl function

--- src/presentation/components/profile/ProfileBody/index.tsx
+++ src/presentation/components/profile/ProfileBody/index.tsx
@@ -1,32 +1,30 @@
 import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { CLOUDINARY_URL } from '../../../../api/cloudinaryIconsAPI';
 import ProfileDescription from '../ProfileDescription';
 import ProfileDetail from '../ProfileDetail';
 import ProfileLogoutButton from '../ProfileLogoutButton';
 import { ThemeContext } from '../../../theme/ThemeContext';
+import { getIconUrl } from '../../../../utils/icon-url';
 
 const ProfileBody = () => {
   const { currentTheme } = useContext(ThemeContext);
-
-  const getIconUrl = (iconName: string) => `${CLOUDINARY_URL}/${iconName}${currentTheme === 'light' ? '' : '_white'}`;
 
   return (
     <View style={styles.container}>
       <ProfileDescription description={'Somos un restaurante de comida rápida con un ambiente familiar y cálido, desde el año 2000 ofrecemos servicio de de restaurante, servicio rápido y servicio a domicilio.'} />
       <ProfileDetail
         label={'Nombre de la empresa'}
-        icon={getIconUrl('users')}
+        icon={getIconUrl('users', currentTheme, true)}
         text={'Restaurante las Rocas'}
       />
       <ProfileDetail
         label={'Categoría'}
-        icon={getIconUrl('restaurant')}
+        icon={getIconUrl('restaurant', currentTheme, true)}
         text={'Restaurante'}
       />
       <ProfileDetail
         label={'Correo corporativo'}
-        icon={getIconUrl('envelope')}
+        icon={getIconUrl('envelope', currentTheme, true)}
         text={'manager@lasrocas.com'}
       />
       <ProfileLogoutButton />
