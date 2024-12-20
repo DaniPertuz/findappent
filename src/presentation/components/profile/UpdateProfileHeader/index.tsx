0b6c6921235f4dd10735e3cab14c8741c4a24ace
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-19 21:00:41 -0500

    UpdateProfileHeader component

---
+++ src/presentation/components/profile/UpdateProfileHeader/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BackButton from '../../ui/BackButton';
+import UpdateProfileHeaderContainer from '../UpdateProfileHeaderContainer';
+import UpdateProfileHeaderTitle from '../UpdateProfileHeaderTitle';
+
+const UpdateProfileHeader = () => {
+  return (
+    <UpdateProfileHeaderContainer>
+      <BackButton />
+      <UpdateProfileHeaderTitle />
+    </UpdateProfileHeaderContainer>
+  );
+};
+
+export default UpdateProfileHeader;
