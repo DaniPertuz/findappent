7b5a5f1d61882ca5fe340fa87c18543e63efde90
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-19 19:34:48 -0500

    ReviewsList component and styles

---
+++ src/presentation/components/dashboard/reviews/ReviewsList/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useContext } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+import ReviewItem from '../ReviewItem';
+import { ThemeContext } from '../../../../theme/ThemeContext';
+
+const ReviewsList: FC<{data: any[]}> = ({ data }) => {
+  const { colors } = useContext(ThemeContext);
+  return (
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <FlatList
+        data={data}
+        contentContainerStyle={styles.listContentContainer}
+        renderItem={({ item }) => <ReviewItem item={item} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  listContentContainer: { paddingHorizontal: 20, gap: 20, paddingBottom: 20 },
+});
+
+export default ReviewsList;
