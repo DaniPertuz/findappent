0bc6825ba18f08bbfeebf5a374a4a2f23feb1417
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-19 20:40:19 -0500

    Update style rules

--- src/presentation/components/profile/ProfileMainContainer/styles.ts
+++ src/presentation/components/profile/ProfileMainContainer/styles.ts
@@ -4,8 +4,6 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     gap: 10,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    paddingBottom: 20,
+    padding: 20,
   },
 });
