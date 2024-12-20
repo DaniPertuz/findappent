9865fc64e590e2062334bba0ebd3a11469d931b5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-19 19:51:04 -0500

    Implement DashboardReviewsModal component

--- src/presentation/components/dashboard/DashboardUserProps/index.tsx
+++ src/presentation/components/dashboard/DashboardUserProps/index.tsx
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import DashboardReviewsModal from '../DashboardReviewsModal';
 import DashboardUserPropItem from './DashboardUserPropItem';
 import { getIconUrl } from '../../../../utils/icon-url';
 import { ThemeContext } from '../../../theme/ThemeContext';
 
 const DashboardUserProps = () => {
   const { currentTheme } = useContext(ThemeContext);
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <DashboardUserPropItem iconUrl={getIconUrl('restaurant', currentTheme, true)} title={'Categoría'} subtitle={'Restaurante'} />
-      <DashboardUserPropItem iconUrl={getIconUrl('heartfavorite', currentTheme, true)} title={'Favoritos'} subtitle={'0 Usuarios'} />
-      <DashboardUserPropItem iconUrl={getIconUrl('starout', currentTheme, true)} title={'Promedio'} subtitle={'0 Usuarios'} />
-    </View>
+    <>
+      <View style={styles.container}>
+        <DashboardUserPropItem iconUrl={getIconUrl('restaurant', currentTheme, true)} title={'Categoría'} subtitle={'Restaurante'} />
+        <DashboardUserPropItem iconUrl={getIconUrl('heartfavorite', currentTheme, true)} title={'Favoritos'} subtitle={'0 Usuarios'} />
+        <DashboardUserPropItem iconUrl={getIconUrl('starout', currentTheme, true)} title={'Promedio'} subtitle={'0 Usuarios'} onPress={() => setModalVisible(true)} />
+      </View>
+      <DashboardReviewsModal modalVisible={modalVisible} setModalVisible={setModalVisible} />
+    </>
   );
 };
 
