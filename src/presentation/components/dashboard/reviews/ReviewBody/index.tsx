f2b3f661d1bc187a8f3e87c53f6c4a3a6f729566
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-19 19:33:45 -0500

    ReviewBody component

---
+++ src/presentation/components/dashboard/reviews/ReviewBody/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC, useContext } from 'react';
+import ReviewBodyContainer from './ReviewBodyContainer';
+import ReviewBodyHeader from './ReviewBodyHeader';
+import ReviewBodyRating from './ReviewBodyRating';
+import { Footnote } from '../../../ui/Footnote';
+import { ThemeContext } from '../../../../theme/ThemeContext';
+
+const ReviewBody: FC = () => {
+  const { colors } = useContext(ThemeContext);
+  return (
+    <ReviewBodyContainer>
+      <ReviewBodyHeader user={'Daniel Pertuz'} time={'Hace 3 meses'} />
+      <ReviewBodyRating rate={3} />
+      <Footnote customColor={colors.mainText}>Comentarios</Footnote>
+    </ReviewBodyContainer>
+  );
+};
+
+export default ReviewBody;
