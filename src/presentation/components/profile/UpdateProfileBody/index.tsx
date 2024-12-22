111d67c3efd8d2d4959e6aa96047eeab74354d6c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-21 20:14:19 -0500

    UpdateProfileBody profile component

---
+++ src/presentation/components/profile/UpdateProfileBody/index.tsx
@@ -0,0 +1,138 @@
+import React, { useContext } from 'react';
+import { Dimensions, Image, KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from 'react-native';
+import WarningMessage from '../../auth/WarningMessage';
+import { Caption2 } from '../../ui/Caption2';
+import { DefaultInput, DescriptionInput, DropdownComponent, PasswordInput } from '../../ui/forms';
+import { getIconUrl } from '../../../../utils/icon-url';
+import { ButtonComponent } from '../../ui/ButtonComponent';
+import { Body } from '../../ui/Body';
+import UpdateProfileFormItemContainer from '../UpdateProfileFormItemContainer';
+import { appStyles } from '../../../theme/app-styles';
+import { ThemeContext } from '../../../theme/ThemeContext';
+
+const deviceHeight = Dimensions.get('window').height;
+
+const UpdateProfileBody = () => {
+  const { colors, currentTheme } = useContext(ThemeContext);
+  return (
+    <KeyboardAvoidingView style={appStyles.keyboardContainer} behavior={(Platform.OS === 'ios') ? 'padding' : 'height'}>
+      <ScrollView contentContainerStyle={styles.contentContainer} keyboardShouldPersistTaps={'always'} showsVerticalScrollIndicator={false}>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Descripción</Caption2>
+          <DescriptionInput description={''} onChange={() => { }} />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Nombre de la empresa</Caption2>
+          <DefaultInput
+            field={''}
+            fieldValue={'name'}
+            placeholder={'Nombre de la empresa'}
+            iconElement={
+              <Image
+                source={{ uri: getIconUrl('users', currentTheme, true) }}
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={'default'}
+            onChange={() => { }} />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Correo corporativo</Caption2>
+          <DefaultInput
+            field={''}
+            fieldValue={'name'}
+            placeholder={'Correo corporativo'}
+            iconElement={
+              <Image
+                source={{ uri: getIconUrl('envelope', currentTheme, true) }}
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={'email-address'}
+            onChange={() => { }}
+          />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Teléfono</Caption2>
+          <DefaultInput
+            field={''}
+            fieldValue={'name'}
+            placeholder={'Teléfono'}
+            iconElement={
+              <Image
+                source={{ uri: getIconUrl('phone', currentTheme, true) }}
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={'phone-pad'}
+            onChange={() => { }} />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Categoría</Caption2>
+          <DropdownComponent items={[{ label: 'Item 1', value: 'Item1' }]} />
+          <DefaultInput
+            field={''}
+            fieldValue={'name'}
+            placeholder={'Escribe tu categoría'}
+            iconElement={
+              <Image
+                source={{ uri: getIconUrl('mall', currentTheme, true) }}
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={'default'}
+            onChange={() => { }}
+          />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Instagram</Caption2>
+          <DefaultInput
+            field={''}
+            fieldValue={'name'}
+            placeholder={'Instagram'}
+            iconElement={
+              <Image
+                source={{ uri: getIconUrl('instagram', currentTheme, true) }}
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={'default'}
+            onChange={() => { }} />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>WhatsApp</Caption2>
+          <DefaultInput
+            field={''}
+            fieldValue={'name'}
+            placeholder={'WhatsApp'}
+            iconElement={
+              <Image
+                source={{ uri: getIconUrl('whatsapp', currentTheme, true) }}
+                style={appStyles.inputIcon}
+              />
+            }
+            keyboardType={'phone-pad'}
+            onChange={() => { }} />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Contraseña</Caption2>
+          <PasswordInput field={''} fieldValue={'password'} placeholder={'Contraseña'} onChange={() => { }} />
+        </UpdateProfileFormItemContainer>
+        <UpdateProfileFormItemContainer>
+          <Caption2 customColor={colors.mainText}>Repetir contraseña</Caption2>
+          <PasswordInput field={''} fieldValue={'password'} placeholder={'Repetir contraseña'} onChange={() => { }} />
+          <WarningMessage text={'Contraseñas no coinciden'} />
+        </UpdateProfileFormItemContainer>
+        <ButtonComponent>
+          <Body customColor={colors.brandWhite}>Guardar cambios</Body>
+        </ButtonComponent>
+      </ScrollView>
+    </KeyboardAvoidingView >
+  );
+};
+
+const styles = StyleSheet.create({
+  contentContainer: { gap: 20, marginTop: 20, paddingBottom: deviceHeight * 0.1 },
+});
+
+export default UpdateProfileBody;
