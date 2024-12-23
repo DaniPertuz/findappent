668d5744a8ae040f220052f583fc7fc0ffa8ddec
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-22 19:09:49 -0500

    Update import and implement AddEditButton component

--- src/presentation/components/profile/ProfileView/ProfileHeaderTitle/index.tsx
+++ src/presentation/components/profile/ProfileView/ProfileHeaderTitle/index.tsx
@@ -1,10 +1,10 @@
 import React, { FC, useContext } from 'react';
-import { View, Pressable, Image } from 'react-native';
-import { CLOUDINARY_URL } from '../../../../api/cloudinaryIconsAPI';
-import { BodySmall } from '../../ui/BodySmall';
-import { H3 } from '../../ui/H3';
-import { ThemeContext } from '../../../theme/ThemeContext';
+import { View } from 'react-native';
+import { BodySmall } from '../../../ui/BodySmall';
+import { H3 } from '../../../ui/H3';
+import { ThemeContext } from '../../../../theme/ThemeContext';
 import { styles } from './styles';
+import AddEditButton from '../../../ui/AddEditButton';
 
 interface Props {
   name: string;
@@ -12,19 +12,15 @@ interface Props {
 }
 
 const ProfileHeaderTitle: FC<Props> = ({ name, email }) => {
-  const { colors, currentTheme } = useContext(ThemeContext);
+  const { colors } = useContext(ThemeContext);
+
   return (
     <>
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <H3 customColor={colors.mainText}>{name}</H3>
         </View>
-        <Pressable>
-          <Image
-            source={{ uri: `${CLOUDINARY_URL}/${currentTheme === 'light' ? 'edit' : 'edit_white'}` }}
-            style={styles.editIcon}
-          />
-        </Pressable>
+        <AddEditButton edit />
       </View>
       <BodySmall customColor={colors.gray}>{email}</BodySmall>
     </>
