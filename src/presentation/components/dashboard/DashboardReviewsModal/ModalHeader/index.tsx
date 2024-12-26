b8d24bcd9f31dea283db822989b0f55862d300ab
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-25 22:17:15 -0500

    Implement ModalProps interface

--- src/presentation/components/dashboard/DashboardReviewsModal/ModalHeader/index.tsx
+++ src/presentation/components/dashboard/DashboardReviewsModal/ModalHeader/index.tsx
@@ -1,15 +1,16 @@
 import React, { FC, useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { ModalProps } from '../../../../../interfaces/modal-props.interface';
 import ModalHideButton from '../ModalHideButton';
 import ModalTitle from '../ModalTitle';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
-const ModalHeader: FC<{ onPress: () => void; }> = ({ onPress }) => {
+const ModalHeader: FC<ModalProps> = ({ closeIcon, hasTitle, onPress }) => {
   const { colors } = useContext(ThemeContext);
   return (
     <View style={[{ backgroundColor: colors.background }, styles.modalContainer]}>
-      <ModalHideButton onPress={onPress} />
-      <ModalTitle />
+      <ModalHideButton closeIcon={closeIcon} onPress={onPress} />
+      {hasTitle && <ModalTitle />}
     </View>
   );
 };
--- src/presentation/components/dashboard/DashboardReviewsModal/index.tsx
+++ src/presentation/components/dashboard/DashboardReviewsModal/index.tsx
@@ -20,7 +20,7 @@ const DashboardReviewsModal: FC<Props> = ({ modalVisible, setModalVisible }) => 
       }}
     >
       <ModalContainer>
-        <ModalHeader onPress={() => setModalVisible(false)} />
+        <ModalHeader hasTitle closeIcon={'down'} onPress={() => setModalVisible(false)} />
         <ReviewsList data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]} />
       </ModalContainer>
     </Modal>
