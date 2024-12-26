241822ec4526b7d26c46ebec9b297590995fec45
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-25 22:23:57 -0500

    Gallery GalleryLoadingModal component to display screen while loading images

---
+++ src/presentation/components/profile/ProfileEdit/gallery/GalleryLoadingModal/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC, useContext } from 'react';
+import { Modal, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { ThemeContext } from '../../../../../theme/ThemeContext';
+
+const GalleryLoadingModal: FC<{ loading: boolean; }> = ({ loading }) => {
+  const { colors } = useContext(ThemeContext);
+  return (
+    <Modal visible={loading} transparent>
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color={colors.darkBlue} />
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+});
+
+export default GalleryLoadingModal;
