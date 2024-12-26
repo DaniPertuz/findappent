845a07b5c2b878f9eef013f8ef97e896eba1bd6c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-25 22:24:34 -0500

    Gallery ItemImage component to handle image in modal

---
+++ src/presentation/components/profile/ProfileEdit/gallery/GalleryItemModal/ItemImage/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useContext } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { ThemeContext } from '../../../../../../theme/ThemeContext';
+
+const ItemImage: FC<{ uri: string; }> = ({ uri }) => {
+  const { colors } = useContext(ThemeContext);
+  return (
+    <View style={[styles.container, { backgroundColor: colors.white }]}>
+      <Image source={{ uri }} style={styles.imageSize} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    borderBottomEndRadius: 16,
+    borderBottomStartRadius: 16,
+    justifyContent: 'center',
+    paddingBottom: 24,
+    paddingHorizontal: 32,
+  },
+  imageSize: {
+    borderRadius: 16,
+    height: 350,
+    width: 350,
+  },
+});
+
+export default ItemImage;
