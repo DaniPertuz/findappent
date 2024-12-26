5466a7585b54a83101f2def6a69f18ccc73d6223
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-25 22:15:09 -0500

    Set icon dynamically

--- src/presentation/components/dashboard/DashboardReviewsModal/ModalHideButton/index.tsx
+++ src/presentation/components/dashboard/DashboardReviewsModal/ModalHideButton/index.tsx
@@ -4,15 +4,16 @@ import { getIconUrl } from '../../../../../utils/icon-url';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
 interface Props {
+  closeIcon: string;
   onPress: () => void;
 }
 
-const ModalHideButton: FC<Props> = ({ onPress }) => {
+const ModalHideButton: FC<Props> = ({ closeIcon, onPress }) => {
   const { currentTheme } = useContext(ThemeContext);
   return (
     <Pressable onPress={onPress}>
       <View style={styles.container}>
-        <Image source={{ uri: getIconUrl('down', currentTheme, true) }} style={styles.size} />
+        <Image source={{ uri: getIconUrl(closeIcon, currentTheme, true) }} style={styles.size} />
       </View>
     </Pressable>
   );
