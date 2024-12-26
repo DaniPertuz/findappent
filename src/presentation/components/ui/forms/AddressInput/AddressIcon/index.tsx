19127ef95332170dd8caa08f0c69dbee5cff23b1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-25 22:42:36 -0500

    Update icon handling

--- src/presentation/components/ui/forms/AddressInput/AddressIcon/index.tsx
+++ src/presentation/components/ui/forms/AddressInput/AddressIcon/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Image } from 'react-native';
+import { getIconUrl } from '../../../../../../utils/icon-url';
+import { ThemeContext } from '../../../../../theme/ThemeContext';
 import { useStyles } from '../../styles';
 
 const AddressIcon = () => {
   const { inputIcon } = useStyles();
+  const { currentTheme } = useContext(ThemeContext);
   return (
-    <Image source={require('../../../../../../assets/icons/mall.png')} style={inputIcon} />
+    <Image source={{ uri: getIconUrl('mall', currentTheme, true) }} style={inputIcon} />
   );
 };
 
