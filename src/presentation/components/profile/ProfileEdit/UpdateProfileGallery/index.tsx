d331c541c7f04fa92b720a6e192bfc443fa2a239
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-12-30 22:26:54 -0500

    Implement GalleryQueryModal component and useGallery hook functions

--- src/presentation/components/profile/ProfileEdit/UpdateProfileGallery/index.tsx
+++ src/presentation/components/profile/ProfileEdit/UpdateProfileGallery/index.tsx
@@ -1,34 +1,39 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useGallery } from '../../../../hooks/useGallery';
 import { AddEditButton, Caption2 } from '../../../ui';
 import GalleryItemsList from '../gallery/GalleryItemsList';
 import GalleryLoadingModal from '../gallery/GalleryLoadingModal';
+import GalleryQueryModal from '../gallery/GalleryQueryModal';
 import UpdateProfileFormItemContainer from '../UpdateProfileFormItemContainer';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
 const UpdateProfileGallery: FC = () => {
   const { colors } = useContext(ThemeContext);
-  const { addGalleryPic, addGalleryPics, loading, placeImages, setPlaceImages } = useGallery();
+  const { addGalleryPics, addPhoto, loading, placeImages, setPlaceImages } = useGallery();
   const isGalleryEmpty = placeImages.length === 0;
+  const [modalVisible, setModalVisible] = useState(false);
 
   const removeImage = (index: number) => {
     setPlaceImages(prevImages => prevImages.filter((_, i) => i !== index));
   };
 
   const handleAddImage = () => {
-    addGalleryPics(100);
+    setModalVisible(true);
   };
 
   return (
-    <UpdateProfileFormItemContainer>
-      <Caption2 customColor={colors.mainText}>Imágenes</Caption2>
-      <View style={styles.container}>
-        {isGalleryEmpty && <AddEditButton onPress={handleAddImage} />}
-        <GalleryItemsList placeImages={placeImages} removeImage={removeImage} />
-        <GalleryLoadingModal loading={loading} />
-      </View>
-    </UpdateProfileFormItemContainer>
+    <>
+      <UpdateProfileFormItemContainer>
+        <Caption2 customColor={colors.mainText}>Imágenes</Caption2>
+        <View style={styles.container}>
+          {isGalleryEmpty && <AddEditButton onPress={handleAddImage} />}
+          <GalleryItemsList placeImages={placeImages} removeImage={removeImage} />
+          <GalleryLoadingModal loading={loading} />
+        </View>
+      </UpdateProfileFormItemContainer>
+      <GalleryQueryModal addGalleryPics={addGalleryPics} addPhoto={addPhoto} modalVisible={modalVisible} setModalVisible={setModalVisible} />
+    </>
   );
 };
 
