2c6b905e46cfdb9077b6da5fc74544b28d661517
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-01-29 10:57:54 -0500

    Update style rules

--- src/presentation/components/ui/forms/DropdownComponent/styles.ts
+++ src/presentation/components/ui/forms/DropdownComponent/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  dropdown: { height: 60, flex: 1, borderWidth: 1 },
+  dropdown: { height: 60, flex: 1, borderWidth: 1, zIndex: 1000 },
 });
