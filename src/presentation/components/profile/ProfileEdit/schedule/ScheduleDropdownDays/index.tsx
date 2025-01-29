436bf2e8ae9f1f41f4bf4cdd299d47dc3c6c0fb6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-01-29 11:03:47 -0500

    Schedule ScheduleSaveButton component

---
+++ src/presentation/components/profile/ProfileEdit/schedule/ScheduleDropdownDays/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { DropdownComponent } from '../../../../ui/forms';
+import { ScheduleType } from '../../../../../../types/schedule.type';
+
+const ScheduleDropdownDays: FC<{ onChange: (value: string | null) => void; }> = ({ onChange }) => {
+  const daysOfWeek = [
+    { label: 'Lunes a Domingo', value: ScheduleType.BusinessDays },
+    { label: 'Lunes a Viernes', value: ScheduleType.Everyday },
+    { label: 'Personalizado', value: ScheduleType.Custom },
+  ];
+
+  return (
+    <DropdownComponent onChange={onChange} placeholder={'Días de atención'} items={daysOfWeek} />
+  );
+};
+
+export default ScheduleDropdownDays;
