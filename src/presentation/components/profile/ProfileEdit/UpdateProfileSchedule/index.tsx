1c13f46eb68522a51d265f3f821e1144542e090f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-01-29 11:19:23 -0500

    Profile UpdateProfileSchedule component

---
+++ src/presentation/components/profile/ProfileEdit/UpdateProfileSchedule/index.tsx
@@ -0,0 +1,52 @@
+import React, { FC, useContext, useEffect, useState } from 'react';
+import { Caption2 } from '../../../ui';
+import CustomizedSchedule from '../schedule/CustomizedSchedule';
+import BusinessDaysSchedule from '../schedule/BusinessDaysSchedule';
+import ScheduleDropdownDays from '../schedule/ScheduleDropdownDays';
+import UpdateProfileFormItemContainer from '../UpdateProfileFormItemContainer';
+import { ScheduleType } from '../../../../../types/schedule.type';
+import { ThemeContext } from '../../../../theme/ThemeContext';
+
+interface Props {
+  onChangeSchedule: (value: string | null) => void;
+}
+
+const UpdateProfileSchedule: FC<Props> = ({ onChangeSchedule }) => {
+  const { colors } = useContext(ThemeContext);
+  const [scheduleType, setScheduleType] = useState<string | null>('');
+
+  const handleScheduleTypeChange = (type: string | null) => {
+    setScheduleType(type);
+
+    if (type === ScheduleType.BusinessDays || type === ScheduleType.Everyday) {
+      onChangeSchedule(null);
+    }
+  };
+
+  useEffect(() => {
+    onChangeSchedule(scheduleType);
+  }, [scheduleType, onChangeSchedule]);
+
+  return (
+    <UpdateProfileFormItemContainer>
+      <Caption2 customColor={colors.mainText}>Horario de atención</Caption2>
+      <ScheduleDropdownDays onChange={handleScheduleTypeChange} />
+      {scheduleType &&
+        <>
+          {scheduleType !== 'custom'
+            ?
+            <BusinessDaysSchedule
+              everyday={scheduleType === ScheduleType.BusinessDays}
+              sendSchedule={() => { }}
+              handleScheduleUpdated={() => { }}
+            />
+            :
+            <CustomizedSchedule everyday sendSchedule={() => { }} handleScheduleUpdated={() => { }} />
+          }
+        </>
+      }
+    </UpdateProfileFormItemContainer>
+  );
+};
+
+export default UpdateProfileSchedule;
