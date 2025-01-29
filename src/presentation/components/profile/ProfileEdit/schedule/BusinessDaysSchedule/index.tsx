beb3adbf551711c2c848f77a84b4364e0765f3a7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-01-29 11:18:49 -0500

    Schedule BusinessDaysSchedule component and styles

---
+++ src/presentation/components/profile/ProfileEdit/schedule/BusinessDaysSchedule/index.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from 'react';
+import { View } from 'react-native';
+import CustomizedSchedule from '../CustomizedSchedule';
+import ScheduleTable from '../ScheduleTable';
+import ScheduleDropdownHours from '../ScheduleDropdownHours';
+import ScheduleEditButton from '../ScheduleEditButton';
+import ScheduleSaveButton from '../ScheduleSaveButton';
+import SchedulerCheckBox from '../SchedulerCheckBox';
+import { useBusinessDaysSchedule } from '../../../../../hooks/useBusinessDaysSchedule';
+import { styles } from './styles';
+
+interface Props {
+  everyday: boolean;
+  sendSchedule: (schedule: string[]) => void;
+  handleScheduleUpdated: (value: boolean) => void;
+}
+
+const BusinessDaysSchedule: FC<Props> = ({ everyday, sendSchedule, handleScheduleUpdated }) => {
+  const {
+    customized,
+    schedule,
+    scheduleUpdated,
+    tableData,
+    onEdit,
+    setClosingHour,
+    setCustomized,
+    setOpeningHour,
+    setScheduleArr,
+    handleUpdateSchedule,
+  } = useBusinessDaysSchedule({ sendSchedule, handleScheduleUpdated, everyday });
+
+  return (
+    <View style={styles.container}>
+      <View style={{ display: (scheduleUpdated || schedule.length !== 0) ? 'none' : 'flex' }}>
+        <SchedulerCheckBox customized={customized} onClick={() => setCustomized(!customized)} />
+      </View>
+      <View style={styles.scheduleContainer}>
+        {(!customized) ? (
+          (schedule.length === 0) ? (
+            <View style={styles.dropdownContainer}>
+              <ScheduleDropdownHours placeholder={'Abre'} onChange={setOpeningHour} />
+              <ScheduleDropdownHours placeholder={'Cierra'} onChange={setClosingHour} />
+              <ScheduleSaveButton onPress={setScheduleArr} />
+            </View>
+          ) : (
+            <View style={[styles.container, styles.gap]}>
+              <ScheduleTable displaySchedule={tableData} />
+              <ScheduleEditButton onPress={onEdit} />
+            </View>
+          )
+        ) : (
+          <View style={styles.container}>
+            <CustomizedSchedule everyday={everyday} sendSchedule={sendSchedule} handleScheduleUpdated={handleUpdateSchedule} />
+          </View>
+        )
+        }
+      </View>
+    </View>
+  );
+};
+
+export default BusinessDaysSchedule;
---
+++ src/presentation/components/profile/ProfileEdit/schedule/BusinessDaysSchedule/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: { flex: 1 },
+  gap: { gap: 10 },
+  dropdownContainer: {
+    gap: 10,
+  },
+  scheduleContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 5,
+  },
+});
