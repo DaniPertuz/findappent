9fdf73098646f75d38920d403483fcbe511ab0ed
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-01-29 11:02:29 -0500

    Schedule ScheduleTable component and styles

---
+++ src/presentation/components/profile/ProfileEdit/schedule/ScheduleTable/index.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { View } from 'react-native';
+import { Row, Rows, Table } from 'react-native-reanimated-table';
+import { ThemeContext } from '../../../../../theme/ThemeContext';
+import { styles } from './styles';
+
+interface Props {
+  displaySchedule: string[][];
+}
+
+const ScheduleTable = ({ displaySchedule }: Props) => {
+  const { colors } = useContext(ThemeContext);
+  const headers: string[] = ['Día', 'Abre', 'Cierra'];
+
+  return (
+    <View style={styles.container}>
+      <Table borderStyle={{ borderColor: colors.mainText, ...styles.tableBorderWidth }}>
+        <Row data={(headers)} style={styles.rowHeight} textStyle={[styles.rowMarginStart, { color: colors.mainText }]} />
+        <Rows data={displaySchedule} textStyle={[styles.rowMargins, { color: colors.mainText }]} />
+      </Table>
+    </View>
+  );
+};
+
+export default ScheduleTable;
---
+++ src/presentation/components/profile/ProfileEdit/schedule/ScheduleTable/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: 10 },
+  rowHeight: { height: 40 },
+  rowMarginStart: { marginStart: 6 },
+  rowMargins: { margin: 6 },
+  tableBorderWidth: { borderWidth: 1 },
+});
