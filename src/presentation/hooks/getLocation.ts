04be5dba79f0d38d034b8aa8f8661b2861eb088e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-02-16 23:09:25 -0500

    Custom hook to get coords from address

---
+++ src/presentation/hooks/getLocation.ts
@@ -0,0 +1,7 @@
+import Geocoder from '../../api/geocoder';
+
+export const getCoords = async (address: string) => {
+  const { results } = await Geocoder.from(address);
+  const { lat, lng } = results[0].geometry.location;
+  return { lat, lng };
+};
