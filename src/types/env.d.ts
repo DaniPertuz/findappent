6fe5e391e09d803116d2314dbc459f655ba9dbe5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-02-16 23:05:18 -0500

    Package react-native-dotenv config

---
+++ src/types/env.d.ts
@@ -0,0 +1,10 @@
+declare module '@env' {
+  export const GOOGLE_MAPS_API_KEY: string;
+  export const API_URL: string;
+  export const API_URL_ANDROID: string;
+  export const API_URL_IOS: string;
+  export const CLOUDINARY_API_KEY: string;
+  export const CLOUDINARY_API_SECRET: string;
+  export const CLOUDINARY_CLOUD_NAME: string;
+  export const CLOUDINARY_UPLOAD_PRESET: string;
+}
