6ac2b2c5b33eeb90172efb8c3baf0a8f2e622bb1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-19 21:05:25 -0500

    Add CLOUDINARY_URL to environment variables and implementation on getIconUrl function

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -7,4 +7,5 @@ declare module '@env' {
   export const CLOUDINARY_API_SECRET: string;
   export const CLOUDINARY_CLOUD_NAME: string;
   export const CLOUDINARY_UPLOAD_PRESET: string;
+  export const CLOUDINARY_URL: string;
 }
--- src/utils/icon-url.ts
+++ src/utils/icon-url.ts
@@ -1,7 +1,8 @@
-import { CLOUDINARY_URL } from '../api/cloudinaryIconsAPI';
+import { CLOUDINARY_URL } from '@env';
 import { ThemeColor } from '../presentation/theme/ThemeContext';
 
 export const getIconUrl = (iconName: string, currentTheme: ThemeColor, hasWhite: boolean) => {
   const url = `${hasWhite ? `${iconName}${currentTheme === 'light' ? '' : '_white'}` : iconName}`;
-  return `${CLOUDINARY_URL}/${url}`;
+  const iconUrl = `${CLOUDINARY_URL}/${url}`;
+  return iconUrl;
 };
