97999d84648362658de13d1782bb610a31952f62
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-19 22:57:32 -0500

    Add useNavigation props to complete app navigation

--- src/presentation/components/profile/ProfileEdit/UpdateProfileHeader/index.tsx
+++ src/presentation/components/profile/ProfileEdit/UpdateProfileHeader/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParams } from '../../../../navigation/MainNavigator';
 import BackButton from '../../../ui/BackButton';
 import UpdateProfileHeaderContainer from '../UpdateProfileHeaderContainer';
 import UpdateProfileHeaderTitle from '../UpdateProfileHeaderTitle';
 
 const UpdateProfileHeader = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
   return (
     <UpdateProfileHeaderContainer>
-      <BackButton />
+      <BackButton onPress={() => navigation.goBack()} />
       <UpdateProfileHeaderTitle />
     </UpdateProfileHeaderContainer>
   );
--- src/presentation/components/profile/ProfileView/ProfileHeaderTitle/index.tsx
+++ src/presentation/components/profile/ProfileView/ProfileHeaderTitle/index.tsx
@@ -1,5 +1,9 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useCallback, useContext } from 'react';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { IPlace } from '../../../../../interfaces/app.interface';
+import { RootStackParams } from '../../../../navigation/MainNavigator';
 import { AddEditButton, BodySmall, H3 } from '../../../ui';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 import { styles } from './styles';
@@ -11,6 +15,26 @@ interface Props {
 
 const ProfileHeaderTitle: FC<Props> = ({ name, email }) => {
   const { colors } = useContext(ThemeContext);
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+
+  const handleEditPress = useCallback(() => {
+    const defaultPlace: IPlace = {
+      name: 'Sample Name',
+      description: 'Sample Description',
+      category: 'Sample Category',
+      address: 'Sample Address',
+      city: 'Sample City',
+      cityState: 'Sample City State',
+      country: 'Sample Country',
+      coords: { latitude: 0, longitude: 0 },
+      email: 'mail@test.com',
+      phone: 3000000,
+      pics: [],
+      schedule: [],
+      status: true,
+    };
+    navigation.navigate('UpdateProfileScreen', { place: defaultPlace });
+  }, [navigation]);
 
   return (
     <>
@@ -18,7 +42,7 @@ const ProfileHeaderTitle: FC<Props> = ({ name, email }) => {
         <View style={styles.titleContainer}>
           <H3 customColor={colors.mainText}>{name}</H3>
         </View>
-        <AddEditButton edit />
+        <AddEditButton edit onPress={handleEditPress} />
       </View>
       <BodySmall customColor={colors.gray}>{email}</BodySmall>
     </>
