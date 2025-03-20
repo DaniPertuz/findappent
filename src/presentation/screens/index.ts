5e0a7b48bb6d3b6ad87cb295e7bc196784237e14
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-19 23:01:25 -0500

    Update exports from index files

--- src/presentation/screens/index.ts
+++ src/presentation/screens/index.ts
@@ -1,3 +1,4 @@
 export * from './auth';
 export { default as DashboardScreen } from './DashboardScreen';
 export { default as SubscriptionScreen } from './SubscriptionScreen';
+export { ProfileScreen, UpdateProfileScreen } from './profile';
