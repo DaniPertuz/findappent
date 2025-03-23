8d7b78c61b5a041efe29e1900e03cbf42408233a
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-23 00:22:50 -0500

    Implement findAPI using AxiosAdapter

---
+++ src/config/api/findapp.api.ts
@@ -0,0 +1,9 @@
+import { API_URL } from '@env';
+import { AxiosAdapter } from '../adapters/axios.adapter';
+
+const findAPI = new AxiosAdapter({
+    baseUrl: API_URL,
+    params: {},
+});
+
+export default findAPI;
