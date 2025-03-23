096793bc968fe87b55c0df2c77f86dcd4fbf9d4d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-23 00:27:26 -0500

    Add interfaces to handle API responses from places, favorites, services, ratings products, and users models

--- src/interfaces/app.interface.ts
+++ src/interfaces/app.interface.ts
@@ -47,6 +47,92 @@ export interface IServiceList {
   services:  IService[];
 }
 
+export interface PlaceResponse {
+  page:   number;
+  limit:  number;
+  total:  number;
+  next:   string | null;
+  prev:   string | null;
+  places: IPlace[];
+}
+
+export interface PlaceAPIResponse {
+  error?:    string;
+  response?: PlaceResponse;
+  status?:   string;
+  place?:    IPlace;
+}
+
+export interface FavoriteResponse {
+  page:      number;
+  limit:     number;
+  total:     number;
+  next:      string | null;
+  prev:      string | null;
+  favorites: IFavorite[];
+}
+
+export interface FavoriteAPIResponse {
+  error?:    string;
+  response?: FavoriteResponse;
+  status?:   string;
+  favorite?: IFavorite;
+}
+
+export interface ServiceResponse {
+  page:     number;
+  limit:    number;
+  total:    number;
+  next:     string | null;
+  prev:     string | null;
+  services: IService[];
+}
+
+export interface ServiceAPIResponse {
+  error?:    string;
+  response?: ServiceResponse;
+  status?:   string;
+  service?:  IService;
+}
+
+export interface RatingResponse {
+  page:    number;
+  limit:   number;
+  total:   number;
+  next:    string | null;
+  prev:    string | null;
+  ratings: IRate[];
+}
+
+export interface RatingAPIResponse {
+  error?:    string;
+  response?: RatingResponse;
+  status?:   string;
+  rating?:   IRate;
+}
+
+export interface ProductResponse {
+  page:     number;
+  limit:    number;
+  total:    number;
+  next:     string | null;
+  prev:     string | null;
+  products: IProduct[];
+}
+
+export interface ProductAPIResponse {
+  error?:    string;
+  response?: ProductResponse;
+  status?:   string;
+  product?:  IProduct;
+}
+
+export interface UserAPIResponse {
+  error?:  string;
+  status?: string;
+  user?:   IUser;
+}
+
 export interface Location {
   latitude:  number;
   longitude: number;
