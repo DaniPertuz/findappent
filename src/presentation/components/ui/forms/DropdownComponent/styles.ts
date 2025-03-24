bc0351525ee9e7802d6c4d3137d781a4313430db
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-23 22:45:33 -0500

    Update style rules

--- src/presentation/components/ui/forms/DropdownComponent/styles.ts
+++ src/presentation/components/ui/forms/DropdownComponent/styles.ts
@@ -2,4 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   dropdown: { height: 60, flex: 1, borderWidth: 1, zIndex: 1000 },
+  iconSize: { height: 30, width: 30 },
+  itemIcon: { flexDirection: 'row', alignItems: 'center', gap: 15, padding: 10 },
+  picture: { borderRadius: 8, height: 42, width: 42 },
 });
