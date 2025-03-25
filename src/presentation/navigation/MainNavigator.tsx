54b7d4c67e6d74b426667ca05ed63ae8961b1dfd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-24 20:54:54 -0500

    Add LoadingScreen to navigation stack

--- src/presentation/navigation/MainNavigator.tsx
+++ src/presentation/navigation/MainNavigator.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { IPlace, IProduct } from '../../interfaces/app.interface';
+import { IProduct, IPlace } from '../../core/entities';
+import { LoadingScreen } from '../screens';
 import { UpdateProfileScreen } from '../screens/profile';
 import BottomTabNavigator from './BottomTabNavigator';
 
 export type RootStackParams = {
   BottomTabNavigator: undefined;
+  LoadingScreen: undefined,
   LoginScreen: undefined,
   MainScreen: undefined,
   NewPasswordScreen: undefined,
@@ -23,6 +25,7 @@ const MainNavigator = () => {
     <Stack.Navigator screenOptions={{ animation: 'none', headerShown: false }}>
       <Stack.Screen name={'BottomTabNavigator'} component={BottomTabNavigator} />
       <Stack.Screen name={'UpdateProfileScreen'} component={UpdateProfileScreen} options={{ presentation: 'transparentModal' }} />
+      <Stack.Screen name={'LoadingScreen'} component={LoadingScreen} />
     </Stack.Navigator>
   );
 };
