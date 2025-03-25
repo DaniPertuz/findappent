4fd26a0eb77e8c465ee8c69832562235d2292d1d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-24 21:03:44 -0500

    Refactor UpdateProfileScreen to use types and pass place parameter to UpdateProfileBody

--- src/presentation/screens/profile/UpdateProfileScreen/index.tsx
+++ src/presentation/screens/profile/UpdateProfileScreen/index.tsx
@@ -1,18 +1,24 @@
-import React, { useContext } from 'react';
-import StatusBarComponent from '../../../components/ui/StatusBarComponent';
+import React, { FC, useContext } from 'react';
+import { StackScreenProps } from '@react-navigation/stack';
+import ProfileMainContainer from '../../../components/profile/ProfileView/ProfileMainContainer';
 import UpdateProfileBody from '../../../components/profile/ProfileEdit/UpdateProfileBody';
 import UpdateProfileHeader from '../../../components/profile/ProfileEdit/UpdateProfileHeader';
-import ProfileMainContainer from '../../../components/profile/ProfileView/ProfileMainContainer';
+import StatusBarComponent from '../../../components/ui/StatusBarComponent';
+import { RootStackParams } from '../../../navigation/MainNavigator';
 import { ThemeContext } from '../../../theme/ThemeContext';
 
-const UpdateProfileScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'UpdateProfileScreen'> { }
+
+const UpdateProfileScreen: FC<Props> = ({ route }) => {
   const { colors, currentTheme } = useContext(ThemeContext);
+  const { place } = route.params;
+
   return (
     <>
       <StatusBarComponent color={colors.background} theme={currentTheme === 'light' ? 'dark-content' : 'light-content'} />
       <ProfileMainContainer>
         <UpdateProfileHeader />
-        <UpdateProfileBody />
+        <UpdateProfileBody place={place} />
       </ProfileMainContainer>
     </>
   );
