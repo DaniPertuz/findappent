6300d3aca78bb4b37dcb7ba226e774391b31c8f1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-24 21:04:14 -0500

    Implement logout functionality in ProfileLogoutButton component

--- src/presentation/components/profile/ProfileView/ProfileLogoutButton/index.tsx
+++ src/presentation/components/profile/ProfileView/ProfileLogoutButton/index.tsx
@@ -1,12 +1,15 @@
 import React, { FC, useContext } from 'react';
 import { Pressable, StyleSheet } from 'react-native';
 import { Body } from '../../../ui';
+import { useAuthStore } from '../../../../../store/authStore';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
 const ProfileLogoutButton: FC = () => {
   const { colors } = useContext(ThemeContext);
+  const logOut = useAuthStore(state => state.logout);
+
   return (
-    <Pressable style={styles.container} onPress={() => console.log('Logout')}>
+    <Pressable style={styles.container} onPress={logOut}>
       <Body customColor={colors.error}>Cerrar sesión</Body>
     </Pressable>
   );
