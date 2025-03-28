7d83817b5894fb1caee6bc87c72dc78fa535dcf5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-27 19:14:24 -0500

    Add screens to stack and update screens animation

--- src/presentation/navigation/MainNavigator.tsx
+++ src/presentation/navigation/MainNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackCardStyleInterpolator } from '@react-navigation/stack';
 import { IProduct, IPlace } from '../../core/entities';
-import { LoadingScreen } from '../screens';
+import { LoadingScreen, LoginScreen, RegisterScreen, ResetPasswordScreen } from '../screens';
 import { UpdateProfileScreen } from '../screens/profile';
 import BottomTabNavigator from './BottomTabNavigator';
 
@@ -20,12 +20,23 @@ export type RootStackParams = {
 
 const Stack = createStackNavigator<RootStackParams>();
 
+const fadeAnimation: StackCardStyleInterpolator = ({ current }) => {
+  return {
+    cardStyle: {
+      opacity: current.progress,
+    },
+  };
+};
+
 const MainNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ animation: 'none', headerShown: false }}>
-      <Stack.Screen name={'BottomTabNavigator'} component={BottomTabNavigator} />
-      <Stack.Screen name={'UpdateProfileScreen'} component={UpdateProfileScreen} options={{ presentation: 'transparentModal' }} />
-      <Stack.Screen name={'LoadingScreen'} component={LoadingScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name={'LoginScreen'} component={LoginScreen} />
+      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name={'RegisterScreen'} component={RegisterScreen} />
+      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name={'NewPasswordScreen'} component={ResetPasswordScreen} />
+      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name={'BottomTabNavigator'} component={BottomTabNavigator} />
+      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name={'UpdateProfileScreen'} component={UpdateProfileScreen} />
+      <Stack.Screen options={{ cardStyleInterpolator: fadeAnimation }} name={'LoadingScreen'} component={LoadingScreen} />
     </Stack.Navigator>
   );
 };
