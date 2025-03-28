851e25dc9423bbfeaa2c729ade5a0cc6b3656244
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-27 19:06:54 -0500

    Add ForgotPasswordButton component to index

--- src/presentation/components/auth/index.ts
+++ src/presentation/components/auth/index.ts
@@ -3,5 +3,6 @@ export { default as AuthButton } from './AuthButton';
 export { default as AuthFooter } from './AuthFooter';
 export { default as AuthFormContainer } from './AuthFormContainer';
 export { default as AuthHeader } from './AuthHeader';
+export { default as ForgotPasswordButton } from './ForgotPasswordButton';
 export { default as RenderInputWithWarning } from './RenderInputWithWarning';
 export { default as WarningMessage } from './WarningMessage';
