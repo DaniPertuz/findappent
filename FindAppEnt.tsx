7d0071102f2b9d18b37807d322871046d5bf1f31
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-27 21:01:35 -0500

    Implement MainNavigator on FindAppEnt

--- FindAppEnt.tsx
+++ FindAppEnt.tsx
@@ -2,16 +2,15 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeContextProvider } from './src/presentation/theme/ThemeContext';
-import Navigation from './src/presentation/navigation/Navigation';
+import MainNavigator from './src/presentation/navigation/MainNavigator';
 import { AuthProvider } from './src/presentation/providers/AuthProvider';
-import { navigationRef } from './src/presentation/navigation/navigationRef';
 
 const FindAppEnt = () => {
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer>
       <AuthProvider>
         <ThemeContextProvider>
-          <Navigation />
+          <MainNavigator />
         </ThemeContextProvider>
       </AuthProvider>
     </NavigationContainer>
