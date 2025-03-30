4cb3a5de84bc2007a9f123765a3511e25f880b13
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-30 18:45:28 -0500

    Include email on URL

--- src/core/use-cases/places/get-place-by-email.use-case.ts
+++ src/core/use-cases/places/get-place-by-email.use-case.ts
@@ -3,7 +3,7 @@ import { IPlace } from '../../entities';
 
 export const getPlaceByEmail = async (email: string): Promise<IPlace> => {
   try {
-    const response = await findAPI.get<IPlace>('/places/email', { params: { email } });
+    const response = await findAPI.get<IPlace>(`places/email/${email}`);
     return response;
   } catch (error) {
     throw new Error(`${error}`);
