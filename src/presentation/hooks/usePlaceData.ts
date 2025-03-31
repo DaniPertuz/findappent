cbc969599a7ac5510ca47902f67db5f04c6167cc
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-30 22:30:40 -0500

    Custom hook usePlaceData to handle places data

---
+++ src/presentation/hooks/usePlaceData.ts
@@ -0,0 +1,32 @@
+
+import { useEffect } from 'react';
+import { usePlaceStore } from '../../store/placeStore';
+import { useAuthStore } from '../../store/authStore';
+
+export const usePlaceData = () => {
+  const { getPlaceByEmail, getFavorites, getRatings, getRatingAverage, place, favorites, ratings, ratingAverage } = usePlaceStore();
+  const user = useAuthStore(state => state.authResponse.user);
+
+  const fetchPlaceData = async () => {
+    await getPlaceByEmail(user?.email!);
+  };
+
+  useEffect(() => {
+    fetchPlaceData();
+  }, [user]);
+
+  useEffect(() => {
+    if (place?._id) {
+      getFavorites(place._id);
+      getRatingAverage(place._id);
+      getRatings(place._id);
+    }
+  }, [place]);
+
+  return {
+    place,
+    favorites,
+    ratingAverage,
+    ratings,
+  };
+};
