1e9849392bf45e5f9a1bf8824ccfd6cb4f226552
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-03-31 00:26:25 -0500

    Implement RenderWarningMessage component for possible errors on login

--- src/presentation/screens/auth/LoginScreen/index.tsx
+++ src/presentation/screens/auth/LoginScreen/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
-import { AuthFormContainer, AuthFooter, AuthHeader, AuthButton, ForgotPasswordButton, RenderInputWithWarning } from '../../../components/auth';
+import { AuthFormContainer, AuthFooter, AuthHeader, AuthButton, ForgotPasswordButton, RenderInputWithWarning, RenderWarningMessage } from '../../../components/auth';
 import { Background, Footnote, MainView, StatusBarComponent } from '../../../components/ui';
 import { useAuthData } from '../../../hooks/useAuthData';
 import { appStyles } from '../../../theme/app-styles';
 import { styles } from './styles';
 
 const LoginScreen = () => {
-  const { colors, email, isFieldValid, loading, logoSource, password, goToRegister, goToResetPassword, onLogin, setEmail, setPassword } = useAuthData();
+  const { colors, email, errorMessage, isFieldValid, loading, logoSource, password, goToRegister, goToResetPassword, onLogin, setEmail, setPassword } = useAuthData();
 
   return (
     <MainView>
@@ -24,6 +24,7 @@ const LoginScreen = () => {
               <AuthHeader title={'Bienvenido'} subtitle={'Ingresa tus credenciales para continuar'} />
               <RenderInputWithWarning label={'Correo corporativo'} value={email} placeholder={'Ingresa tu correo'} onChange={setEmail} isEmail isFilled={isFieldValid} />
               <RenderInputWithWarning label={'Contraseña'} value={password} placeholder={'Ingresa tu contraseña'} onChange={setPassword} isPassword isFilled={isFieldValid} />
+              {errorMessage && <RenderWarningMessage errorMessage={errorMessage} />}
               <ForgotPasswordButton onPress={goToResetPassword} />
               <AuthButton loading={loading} text={'Iniciar sesión'} onPress={onLogin} />
               <AuthFooter label={'¿No tienes una empresa registrada?'} link={'Crear cuenta'} onPress={goToRegister} />
