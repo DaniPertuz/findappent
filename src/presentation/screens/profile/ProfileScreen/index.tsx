51764e2fbbba8a28572ebd0059b9bfe9594e66e3
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-04 09:48:36 -0500

    Pass user as prop to ProfileHeader component

--- src/presentation/screens/profile/ProfileScreen/index.tsx
+++ src/presentation/screens/profile/ProfileScreen/index.tsx
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
-import StatusBarComponent from '../../../components/ui/StatusBarComponent';
 import ProfileBody from '../../../components/profile/ProfileView/ProfileBody';
 import ProfileHeader from '../../../components/profile/ProfileView/ProfileHeader';
 import ProfileMainContainer from '../../../components/profile/ProfileView/ProfileMainContainer';
+import StatusBarComponent from '../../../components/ui/StatusBarComponent';
+import { useAuthStore } from '../../../../store/authStore';
 import { ThemeContext } from '../../../theme/ThemeContext';
 
 const ProfileScreen = () => {
   const { colors, currentTheme } = useContext(ThemeContext);
+  const user = useAuthStore(state => state.authResponse.user);
+
   return (
     <>
       <StatusBarComponent color={colors.background} theme={currentTheme === 'light' ? 'dark-content' : 'light-content'} />
       <ProfileMainContainer>
-        <ProfileHeader />
+        <ProfileHeader user={user} />
         <ProfileBody />
       </ProfileMainContainer>
     </>
