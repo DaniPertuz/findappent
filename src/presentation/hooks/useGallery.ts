52fb2b1c3653f6d322c97d9b4100a13dd3444f3c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-04 09:46:03 -0500

    Add functions to select and upload user profile picture

--- src/presentation/hooks/useGallery.ts
+++ src/presentation/hooks/useGallery.ts
@@ -1,11 +1,34 @@
 import { useState } from 'react';
 import { ImagePickerResponse } from 'react-native-image-picker';
 import { ImagePickerAdapter } from '../../adapters/ImagePickerAdapter';
+import { useAuthStore } from '../../store/authStore';
+import { usePlaceStore } from '../../store/placeStore';
+import { handleUpdateCloudinaryPic } from './useCloudinaryOperation';
 
 export const useGallery = () => {
+  const [profilePicture, setProfilePicture] = useState<ImagePickerResponse>();
   const [placeImages, setPlaceImages] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState<ImagePickerResponse>();
+  const user = useAuthStore(state => state.authResponse.user);
+  const setUser = useAuthStore(state => state.setUser);
+  const { place, updatePlacePhoto, updateUserPhoto } = usePlaceStore();
+
+  const selectProfilePicture = async () => {
+    const picture = await ImagePickerAdapter.getPicturesFromLibrary(1);
+
+    if (!picture || !picture.assets) { return; }
+
+    setProfilePicture(picture);
+  };
+
+  const takeProfilePicture = async () => {
+    const picture = await ImagePickerAdapter.takePicture();
+
+    if (!picture || !picture.assets) { return; }
+
+    setProfilePicture(picture);
+  };
 
   const addGalleryPic = async () => {
     setLoading(true);
@@ -59,13 +82,47 @@ export const useGallery = () => {
     }
   };
 
+  const uploadPicture = async () => {
+    setLoading(true);
+    try {
+      if (!profilePicture) {
+        setLoading(false);
+        return;
+      }
+
+      const img = await handleUpdateCloudinaryPic(profilePicture, user?._id!, true, user?.photo);
+
+      const [respPlace, respUser] = await Promise.all([
+        updatePlacePhoto(place?._id!, img[0]),
+        updateUserPhoto(user?._id!, img[0]),
+      ]);
+
+      if (!respPlace || !respUser) {
+        setLoading(false);
+        return;
+      }
+      if (respUser) {
+        setUser(respUser);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     loading,
     placeImages,
+    profilePicture,
     response,
     addGalleryPic,
     addGalleryPics,
     addPhoto,
+    selectProfilePicture,
     setPlaceImages,
+    setProfilePicture,
+    takeProfilePicture,
+    uploadPicture,
   };
 };
