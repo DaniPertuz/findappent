65365544a1a569a9d8b85b909db030eae03f535d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-04 09:55:05 -0500

    Refactor ProfilePicture component to accept photoUri as a prop

--- src/presentation/components/profile/ProfileView/ProfilePicture/index.tsx
+++ src/presentation/components/profile/ProfileView/ProfilePicture/index.tsx
@@ -1,20 +1,20 @@
 import React, { FC, useContext } from 'react';
 import { Image, ImageStyle, Pressable, StyleProp } from 'react-native';
-import { useAuthStore } from '../../../../../store/authStore';
 import { getIconUrl } from '../../../../../utils/icon-url';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
 interface Props {
+  photoUri: string;
   onPress?: () => void;
   styles: StyleProp<ImageStyle>;
 }
 
-const ProfilePicture: FC<Props> = ({ onPress, styles }) => {
+const ProfilePicture: FC<Props> = ({ photoUri, onPress, styles }) => {
   const { currentTheme } = useContext(ThemeContext);
-  const user = useAuthStore(state => state.authResponse.user);
+
   return (
     <Pressable onPress={onPress}>
-      <Image source={{ uri: user?.photo ?? getIconUrl('fa_blue', currentTheme, false) }} style={styles} />
+      <Image source={{ uri: photoUri ?? getIconUrl('fa_blue', currentTheme, false) }} style={styles} />
     </Pressable>
   );
 };
