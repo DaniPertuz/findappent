eb31e51694b30c49249dfac1fbcdbe3c4756e693
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-05 16:43:13 -0500

    Replace View with ScrollView in ProfileMainContainer for better scrolling behavior and update styles for improved layout

--- src/presentation/components/profile/ProfileView/ProfileMainContainer/index.tsx
+++ src/presentation/components/profile/ProfileView/ProfileMainContainer/index.tsx
@@ -1,14 +1,14 @@
 import React, { FC, PropsWithChildren, useContext } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import { styles } from './styles';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
 const ProfileMainContainer: FC<PropsWithChildren> = ({ children }) => {
   const { colors } = useContext(ThemeContext);
   return (
-    <View style={[{ backgroundColor: colors.background }, styles.container]}>
+    <ScrollView contentContainerStyle={[{ backgroundColor: colors.background }, styles.container]} showsVerticalScrollIndicator={false}>
       {children}
-    </View>
+    </ScrollView>
   );
 };
 
