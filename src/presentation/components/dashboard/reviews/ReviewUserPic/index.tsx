3b2aaccd44c016dcd68ec8566b5d4b501fd1ec1e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-05 17:10:46 -0500

    Update component params to receive user main photo dinamically

--- src/presentation/components/dashboard/reviews/ReviewUserPic/index.tsx
+++ src/presentation/components/dashboard/reviews/ReviewUserPic/index.tsx
@@ -3,11 +3,11 @@ import { Image, StyleSheet } from 'react-native';
 import { getIconUrl } from '../../../../../utils/icon-url';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
-const ReviewUserPic: FC = () => {
+const ReviewUserPic: FC<{ photoUrl?: string }> = ({ photoUrl }) => {
   const { currentTheme } = useContext(ThemeContext);
   return (
     <Image
-      source={{ uri: getIconUrl('fa_blue', currentTheme, false) }}
+      source={{ uri: photoUrl ?? getIconUrl('fa_blue', currentTheme, false) }}
       style={styles.picture}
     />
   );
