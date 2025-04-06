a5da4b283e224acd78b0d41908bcd2e5ed32cd1a
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-06 01:23:06 -0500

    Refactor ReviewsList to use RatingResponse and integrate ReviewPagination for improved navigation

--- src/presentation/components/dashboard/reviews/ReviewsList/index.tsx
+++ src/presentation/components/dashboard/reviews/ReviewsList/index.tsx
@@ -1,25 +1,30 @@
 import React, { FC, useContext } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
-import { IRate } from '../../../../../core/entities';
 import ReviewItem from '../ReviewItem';
+import { deviceHeight } from '../../../../../utils/dimensions';
+import { RatingResponse } from '../../../../../interfaces/app.interface';
+import ReviewPagination from '../ReviewPagination';
 import { ThemeContext } from '../../../../theme/ThemeContext';
 
-const ReviewsList: FC<{ data: IRate[]; }> = ({ data }) => {
+const ReviewsList: FC<{ data: RatingResponse; }> = ({ data }) => {
   const { colors } = useContext(ThemeContext);
+  const { ratings, next, prev, page } = data;
   return (
     <View style={[styles.container, { backgroundColor: colors.background }]}>
       <FlatList
-        data={data}
+        data={ratings}
         contentContainerStyle={styles.listContentContainer}
         renderItem={({ item }) => <ReviewItem item={item} />}
+        showsVerticalScrollIndicator={false}
       />
+      <ReviewPagination prev={prev} next={next} page={page} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  listContentContainer: { paddingHorizontal: 20, gap: 20, marginVertical: 20 },
+  container: { height: deviceHeight * 0.85 },
+  listContentContainer: { paddingHorizontal: 20, gap: 20 },
 });
 
 export default ReviewsList;
