e47c3741a7f9eab52b15019dfa9b5c3314c6280e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-04-07 22:38:50 -0500

    Refactor updateUserPasswordUseCase to return user data instead of void

--- src/core/use-cases/users/update-user-password.use-case.ts
+++ src/core/use-cases/users/update-user-password.use-case.ts
@@ -1,9 +1,10 @@
 import findAPI from '../../../config/api/findapp.api';
 import { IUser } from '../../entities';
 
-export const updateUserPasswordUseCase = async (email: string, password: string): Promise<void> => {
+export const updateUserPasswordUseCase = async (email: string, password: string): Promise<IUser> => {
   try {
-    await findAPI.patch<IUser>('users/user/password', { email, password });
+    const resp = await findAPI.patch<IUser>('users/user/password', { email, password });
+    return resp;
   } catch (error) {
     throw new Error(`${error}`);
   }
