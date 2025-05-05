ca4a712dda46e1d95772d2061f1bc6853b20f979
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-05-04 23:25:34 -0500

    Update function params to improve Cloudinary images upload

--- src/core/use-cases/places/upload-pics.use-case.ts
+++ src/core/use-cases/places/upload-pics.use-case.ts
@@ -1,4 +1,4 @@
 import { ImagePickerResponse } from 'react-native-image-picker';
 import { handleUpdateCloudinaryPic } from '../../../presentation/hooks/useCloudinaryOperation';
 
-export const uploadPics = async (data: ImagePickerResponse): Promise<string[]> => await handleUpdateCloudinaryPic(data);
+export const uploadPics = async (data: ImagePickerResponse, userId: string): Promise<string[]> => await handleUpdateCloudinaryPic(data, userId, false, false, undefined);
