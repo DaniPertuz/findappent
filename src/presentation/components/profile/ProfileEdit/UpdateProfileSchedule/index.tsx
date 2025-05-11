25105de6625015777bdea8c6312c7154182be5b2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-05-11 17:16:54 -0500

    Refactor UpdateProfileSchedule component to improve schedule handling and UI interactions

--- src/presentation/components/profile/ProfileEdit/UpdateProfileSchedule/index.tsx
+++ src/presentation/components/profile/ProfileEdit/UpdateProfileSchedule/index.tsx
@@ -1,48 +1,67 @@
-import React, { FC, useContext, useEffect, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
+import { View } from 'react-native';
 import { Caption2 } from '../../../ui';
 import CustomizedSchedule from '../schedule/CustomizedSchedule';
 import BusinessDaysSchedule from '../schedule/BusinessDaysSchedule';
 import ScheduleDropdownDays from '../schedule/ScheduleDropdownDays';
 import UpdateProfileFormItemContainer from '../UpdateProfileFormItemContainer';
+import { IPlace } from '../../../../../core/entities';
 import { ScheduleType } from '../../../../../interfaces/app.interface';
 import { ThemeContext } from '../../../../theme/ThemeContext';
+import ScheduleTable from '../schedule/ScheduleTable';
+import ScheduleEditButton from '../schedule/ScheduleEditButton';
 
 interface Props {
-  onChangeSchedule: (value: string | null) => void;
+  place: IPlace;
+  onChangeSchedule: (value: string[]) => void;
 }
 
-const UpdateProfileSchedule: FC<Props> = ({ onChangeSchedule }) => {
+const UpdateProfileSchedule: FC<Props> = ({ place, onChangeSchedule }) => {
+  const displaySchedule = place.schedule.map(schedule => schedule.split(' ').map(s => s.replace('AM', ' AM').replace('PM', ' PM')));
+  const isScheduleAvailable = place.schedule.length !== 0;
+  const gap = { gap: 10 };
   const { colors } = useContext(ThemeContext);
+  const [editSchedule, setEditSchedule] = useState(isScheduleAvailable);
+  const [isScheduleUpdated, setIsScheduleUpdated] = useState(false);
   const [scheduleType, setScheduleType] = useState<string | null>('');
 
   const handleScheduleTypeChange = (type: string | null) => {
     setScheduleType(type);
-
-    if (type === ScheduleType.BusinessDays || type === ScheduleType.Everyday) {
-      onChangeSchedule(null);
-    }
   };
 
-  useEffect(() => {
-    onChangeSchedule(scheduleType);
-  }, [scheduleType, onChangeSchedule]);
+  const handleEditSchedule = (edited: boolean) => {
+    setIsScheduleUpdated(edited);
+  };
+
+  const handleEditPress = () => {
+    setEditSchedule(!editSchedule);
+    setIsScheduleUpdated(false);
+  };
 
   return (
     <UpdateProfileFormItemContainer>
       <Caption2 customColor={colors.mainText}>Horario de atención</Caption2>
-      <ScheduleDropdownDays onChange={handleScheduleTypeChange} />
-      {scheduleType &&
+      {editSchedule
+        ?
+        <View style={gap}>
+          <ScheduleTable displaySchedule={displaySchedule} />
+          <ScheduleEditButton onPress={handleEditPress} />
+        </View>
+        :
         <>
-          {scheduleType !== 'custom'
-            ?
-            <BusinessDaysSchedule
-              everyday={scheduleType === ScheduleType.BusinessDays}
-              sendSchedule={() => { }}
-              handleScheduleUpdated={() => { }}
-            />
-            :
-            <CustomizedSchedule everyday sendSchedule={() => { }} handleScheduleUpdated={() => { }} />
-          }
+          {!isScheduleUpdated && <ScheduleDropdownDays onChange={handleScheduleTypeChange} />}
+          <View>
+            {scheduleType !== 'custom'
+              ?
+              <BusinessDaysSchedule
+                everyday={scheduleType === ScheduleType.BusinessDays}
+                sendSchedule={onChangeSchedule}
+                handleScheduleUpdated={handleEditSchedule}
+              />
+              :
+              <CustomizedSchedule everyday sendSchedule={onChangeSchedule} handleScheduleUpdated={handleEditSchedule} />
+            }
+          </View>
         </>
       }
     </UpdateProfileFormItemContainer>
