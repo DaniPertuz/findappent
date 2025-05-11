8f74e6de42da3a9279ae635360079c05dde24c19
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-05-11 17:14:01 -0500

    Convert schedule times to 12-hour format in ScheduleTable component

--- src/presentation/components/profile/ProfileEdit/schedule/ScheduleTable/index.tsx
+++ src/presentation/components/profile/ProfileEdit/schedule/ScheduleTable/index.tsx
@@ -12,11 +12,32 @@ const ScheduleTable: FC<Props> = ({ displaySchedule }) => {
   const { colors } = useContext(ThemeContext);
   const headers: string[] = ['Día', 'Abre', 'Cierra'];
 
+  const convertTo12HourFormat = (time: string) => {
+    const [hour, minute] = time.split(':').map((part) => parseInt(part, 10));
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+    const hour12 = hour % 12 || 12;
+    const minutes = minute.toString().padStart(2, '0');
+    return `${hour12}:${minutes} ${ampm}`;
+  };
+
+  const isValidHour = (time: string) => {
+    return !isNaN(parseInt(time.split(':')[0], 10));
+  };
+
+  const convertedSchedule = displaySchedule.map((row) => {
+    const [day, openTime, closeTime] = row;
+
+    const convertedOpenTime = isValidHour(openTime) ? convertTo12HourFormat(openTime) : openTime;
+    const convertedCloseTime = isValidHour(closeTime) ? convertTo12HourFormat(closeTime) : closeTime;
+
+    return [day, convertedOpenTime, convertedCloseTime];
+  });
+
   return (
     <View style={styles.container}>
       <Table borderStyle={{ borderColor: colors.mainText, ...styles.tableBorderWidth }}>
         <Row data={(headers)} style={styles.rowHeight} textStyle={[styles.rowMarginStart, { color: colors.mainText }]} />
-        <Rows data={displaySchedule} textStyle={[styles.rowMargins, { color: colors.mainText }]} />
+        <Rows data={convertedSchedule} textStyle={[styles.rowMargins, { color: colors.mainText }]} />
       </Table>
     </View>
   );
