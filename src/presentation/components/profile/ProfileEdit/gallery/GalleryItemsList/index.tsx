aac72373b7fc36000b4cc7c81763381cf0b8bab1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-05-24 18:08:30 -0500

    Refactor GalleryItemsList to include onAddImage prop and enhance layout styling

--- src/presentation/components/profile/ProfileEdit/gallery/GalleryItemsList/index.tsx
+++ src/presentation/components/profile/ProfileEdit/gallery/GalleryItemsList/index.tsx
@@ -1,14 +1,18 @@
 import React, { FC, useEffect, useRef } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { AddEditButton } from '../../../../ui';
+import { usePlaceStore } from '../../../../../../store';
 import GalleryItem from '../GalleryItem';
 
 interface Props {
   placeImages: string[];
   removeImage: (index: number) => void;
+  onAddImage: () => void;
 }
 
-const GalleryItemsList: FC<Props> = ({ placeImages, removeImage }) => {
+const GalleryItemsList: FC<Props> = ({ placeImages, removeImage, onAddImage }) => {
   const scrollViewRef = useRef<ScrollView>(null);
+  const place = usePlaceStore(state => state.place);
 
   useEffect(() => {
     if (placeImages.length > 0) {
@@ -17,12 +21,17 @@ const GalleryItemsList: FC<Props> = ({ placeImages, removeImage }) => {
   }, [placeImages]);
 
   return (
-    <ScrollView horizontal ref={scrollViewRef} showsHorizontalScrollIndicator={false}>
+    <ScrollView contentContainerStyle={styles.container} horizontal ref={scrollViewRef} showsHorizontalScrollIndicator={false}>
       {placeImages.map((image, index) => (
         <GalleryItem key={index} uri={image} onPress={() => removeImage(index)} />
       ))}
+      {place?.premium === 2 && placeImages.length < 2 && <AddEditButton onPress={onAddImage} />}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flexDirection: 'row', alignItems: 'center', gap: 10 },
+});
 
 export default GalleryItemsList;
