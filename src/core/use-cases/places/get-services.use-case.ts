f55cc3904c597b25037e9a52060ca523e4cd8a43
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-05 22:34:12 -0500

    Refactor getServicesUseCase to adapt API response structure

--- src/core/use-cases/places/get-services.use-case.ts
+++ src/core/use-cases/places/get-services.use-case.ts
@@ -1,10 +1,21 @@
 import findAPI from '../../../config/api/findapp.api';
 import { ServiceAPIResponse, ServiceResponse } from '../../../interfaces/app.interface';
+import { IService } from '../../entities';
+
+const adaptServiceResponse = (data: any): ServiceResponse => ({
+  page: data.page,
+  limit: data.limit,
+  total: data.total,
+  next: data.next,
+  prev: data.prev,
+  services: data.journeys as IService[],
+});
 
 export const getServicesUseCase = async (placeId: string): Promise<ServiceAPIResponse> => {
   try {
-    const response = await findAPI.get<ServiceResponse>(`/services/place/${placeId}`);
-    return { response };
+    const response = await findAPI.get<ServiceResponse>(`journeys/place/${placeId}`);
+    const adaptedResponse = adaptServiceResponse(response);
+    return { response: adaptedResponse };
   } catch (error: any) {
     throw new Error(`${error}`);
   }
