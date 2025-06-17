9a05233826dd62d5133df9d9356601f05e13dc9d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-17 01:28:18 -0500

    Component DashboardStatistics and styles to display dashboard graphics

---
+++ src/presentation/components/dashboard/DashboardStatistics/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react';
+import { ScrollView, View } from 'react-native';
+import { useChartDropdownData } from '../../../hooks/useChartDropdownData';
+import { Subheadline } from '../../ui';
+import { KeywordsChart, SearchChart, UsersChart } from '../charts';
+import DropdownCharts from '../charts/components/DropdownCharts';
+import { styles } from './styles';
+
+const DashboardStatistics: FC = () => {
+  const {
+    colors,
+    isLoading,
+    monthsAvailable,
+    selectedMonth,
+    selectedYear,
+    yearsAvailable,
+    setSelectedMonth,
+    setSelectedYear,
+  } = useChartDropdownData();
+
+  return (
+    <View style={styles.container}>
+      <Subheadline customColor={colors.mainText}>Estadísticas</Subheadline>
+      <ScrollView contentContainerStyle={styles.scrollContentContainer} style={styles.scrollContainer} showsVerticalScrollIndicator={false}>
+        <View style={styles.chartContainer}>
+          <Subheadline customColor={colors.mainText}>Búsquedas mensuales</Subheadline>
+          <DropdownCharts
+            colors={colors}
+            isLoading={isLoading}
+            monthsAvailable={monthsAvailable}
+            yearsAvailable={yearsAvailable}
+            setSelectedMonth={setSelectedMonth}
+            setSelectedYear={setSelectedYear}
+          />
+          <SearchChart selectedMonth={selectedMonth} selectedYear={selectedYear} />
+        </View>
+        <View style={styles.chartContainer}>
+          <Subheadline customColor={colors.mainText}>Usuarios frecuentes</Subheadline>
+          <UsersChart />
+        </View>
+        <View style={styles.chartContainer}>
+          <Subheadline customColor={colors.mainText}>Palabras clave</Subheadline>
+          <KeywordsChart selectedMonth={selectedMonth} selectedYear={selectedYear} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default DashboardStatistics;
---
+++ src/presentation/components/dashboard/DashboardStatistics/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  chartContainer: {
+    gap: 20,
+  },
+  container: {
+    flex: 1,
+    gap: 40,
+  },
+  scrollContainer: {
+    flex: 1,
+  },
+  scrollContentContainer: {
+    gap: 40,
+  },
+});
