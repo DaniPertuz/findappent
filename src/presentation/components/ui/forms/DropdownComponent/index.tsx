d03bc547354940ff5ab2a350deed3f0d485f3f16
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-17 01:25:52 -0500

    Add support for initial value on DropdownComponent and improve icon handling

--- src/presentation/components/ui/forms/DropdownComponent/index.tsx
+++ src/presentation/components/ui/forms/DropdownComponent/index.tsx
@@ -11,21 +11,29 @@ interface ItemType {
 }
 
 interface Props {
+  initialValue?: string | null;
   items: ItemType[];
   placeholder: string;
   onChange: (value: string | null) => void;
 }
 
-const DropdownComponent: FC<Props> = ({ items, placeholder, onChange }) => {
+const ArrowDownIcon: FC<{ color: string }> = ({ color }) => <DropDownIcon color={color} />;
+
+const DropdownComponent: FC<Props> = ({ initialValue, items, placeholder, onChange }) => {
   const { colors } = useContext(ThemeContext);
+  const defaultValue = items.find(item => item.value === initialValue?.toLowerCase())?.value ?? null;
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState<string | null>(defaultValue);
   const [list, setList] = useState(
     items.map((item) => ({
       label: `${item.label}`,
       value: item.value,
+      // icon: () => <Image source={{ uri: getIconUrl(item.value, currentTheme, true) }} style={styles.iconSize} />,
     }))
   );
+
+  const setArrowDownIcon = () => <ArrowDownIcon color={colors.mainText} />;
+  const setArrowUpIcon = () => <DropDownUpIcon color={colors.mainText} />;
 
   return (
     <DropDownPicker
@@ -38,11 +46,17 @@ const DropdownComponent: FC<Props> = ({ items, placeholder, onChange }) => {
       setValue={setValue}
       setItems={setList}
       placeholder={placeholder}
-      ArrowDownIconComponent={DropDownIcon}
-      ArrowUpIconComponent={DropDownUpIcon}
+      ArrowDownIconComponent={setArrowDownIcon}
+      ArrowUpIconComponent={setArrowUpIcon}
       style={[styles.dropdown, { backgroundColor: colors.white, borderColor: colors.mainText }]}
       textStyle={{ color: colors.mainText }}
       dropDownContainerStyle={{ backgroundColor: colors.white, borderColor: colors.mainText }}
+      // renderListItem={({ item }) => (
+      //   <View style={item.icon ? styles.itemIcon : null}>
+      //     {item.icon ? item.icon() : null}
+      //     <Caption1 customColor={colors.mainText}>{item.label}</Caption1>
+      //   </View>
+      // )}
     />
   );
 };
