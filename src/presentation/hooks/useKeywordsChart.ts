20626033665b4fe5c4ac998a2b7cfa1c870f5156
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-17 01:26:14 -0500

    Refactor useKeywordsChart to fetch and process keyword data by month and year

--- src/presentation/hooks/useKeywordsChart.ts
+++ src/presentation/hooks/useKeywordsChart.ts
@@ -1,48 +1,61 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChartDataItem, PieDataItem } from '../../interfaces/app.interface';
+import { useSearchStore } from '../../store/searchStore';
 import { getRandomColor } from '../../utils/randomColor';
 import { usePlaceData } from './usePlaceData';
 import { ThemeContext } from '../theme/ThemeContext';
 
-export const useKeywordsChart = () => {
+export const useKeywordsChart = (selectedMonth: string, selectedYear: string) => {
   const { colors } = useContext(ThemeContext);
-  const { services } = usePlaceData();
-  const searchCounts: Record<string, number> = {};
+  const [pieData, setPieData] = useState<PieDataItem[]>([]);
+  const [chartData, setChartData] = useState<ChartDataItem[]>([]);
+  const { place } = usePlaceData();
+  const getSearchLogs = useSearchStore(state => state.getSearchLogs);
 
-  services.services.forEach(item => {
-    if (item.search) {
-      searchCounts[item.search] = (searchCounts[item.search] || 0) + 1;
-    }
-  });
+  useEffect(() => {
+    const fetchLogs = async () => {
+      if (!selectedMonth || !selectedYear || !place?._id) { return; }
 
-  const unique = Object.keys(searchCounts).map(search => ({
-    search,
-    total: searchCounts[search],
-  }));
+      try {
+        const data = await getSearchLogs(+selectedYear, +selectedMonth, place._id);
 
-  const pieDataRaw = unique.map((item) => ({
-    value: item.total,
-    name: item.search,
-    // name: item.search.length > 20 ? item.search.slice(0, 20) + '...' : item.search,
-  }));
+        const pieDataRaw = data.keywords.map((item) => ({
+          value: item.count,
+          name: item.keyword,
+          // name: item.search.length > 20 ? item.search.slice(0, 20) + '...' : item.search,
+        }));
 
-  const total = pieDataRaw.reduce((sum, item) => sum + item.value, 0);
+        const total = data.total;
 
-  const pieData = pieDataRaw.map((item, index) => ({
-    ...item,
-    text: ((item.value / total) * 100).toFixed(1) + '%',
-    color: getRandomColor(index),
-  }));
+        const pieDataProcessed = pieDataRaw.map((item, index) => ({
+          ...item,
+          text: ((item.value / total) * 100).toFixed(1) + '%',
+          color: getRandomColor(index),
+        }));
 
-  const data = pieData.sort((a, b) => b.value - a.value).map((item) => ({
-    name: item.name,
-    amount: item.value,
-    color: item.color,
-    legendFontColor: colors.mainText,
-    legendFontSize: 12,
-  }));
+        const chartDataProcessed = pieDataProcessed
+          .sort((a, b) => b.value - a.value)
+          .map((item) => ({
+            name: item.name,
+            amount: item.value,
+            color: item.color,
+            legendFontColor: colors.mainText,
+            legendFontSize: 12,
+          }));
+
+        setChartData(chartDataProcessed);
+        setPieData(pieDataProcessed);
+      } catch (error) {
+        console.error('Error fetching monthly logs:', error);
+      }
+    };
+
+    fetchLogs();
+  }, [selectedMonth, selectedYear, place, getSearchLogs, colors.mainText]);
+
   return {
     colors,
     pieData,
-    data,
+    data: chartData,
   };
 };
