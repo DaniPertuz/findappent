44c21018fb20c62d96a90cfc9b41dc7ddfa82873
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-17 01:20:19 -0500

    Refactor DropdownIcon and DropDownUpIcon components to accept color prop and improve styling

--- src/presentation/components/ui/forms/DropdownComponent/DropDownUpIcon/index.tsx
+++ src/presentation/components/ui/forms/DropdownComponent/DropDownUpIcon/index.tsx
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react';
+import React, { FC, useContext } from 'react';
 import { Image } from 'react-native';
 import { getIconUrl } from '../../../../../../utils/icon-url';
 import { appStyles } from '../../../../../theme/app-styles';
 import { ThemeContext } from '../../../../../theme/ThemeContext';
 
-const DropDownUpIcon = () => {
-  const { currentTheme } = useContext(ThemeContext);
+const DropDownUpIcon: FC<{ color?: string }> = ({ color }) => {
+  const { colors, currentTheme } = useContext(ThemeContext);
   return (
     <Image
       source={{ uri: getIconUrl('down', currentTheme, true) }}
-      style={[appStyles.inputIcon, { transform: [{ rotate: '180deg' }]}]}
+      style={[appStyles.inputIcon, { transform: [{ rotate: '180deg' }]}, { tintColor: color || colors.black }]}
     />
   );
 };
--- src/presentation/components/ui/forms/DropdownComponent/DropdownIcon/index.tsx
+++ src/presentation/components/ui/forms/DropdownComponent/DropdownIcon/index.tsx
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react';
+import React, { FC, useContext } from 'react';
 import { Image } from 'react-native';
 import { getIconUrl } from '../../../../../../utils/icon-url';
 import { appStyles } from '../../../../../theme/app-styles';
 import { ThemeContext } from '../../../../../theme/ThemeContext';
 
-const DropDownIcon = () => {
-  const { currentTheme } = useContext(ThemeContext);
+const DropDownIcon: FC<{ color?: string }> = ({ color }) => {
+  const { colors, currentTheme } = useContext(ThemeContext);
   return (
     <Image
       source={{ uri: getIconUrl('down', currentTheme, true) }}
-      style={appStyles.inputIcon}
+      style={[appStyles.inputIcon, { tintColor: color || colors.black }]}
     />
   );
 };
