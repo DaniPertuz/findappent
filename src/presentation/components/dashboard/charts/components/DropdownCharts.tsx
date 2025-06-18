56d4194df8296d546742838a8c78366135ec8066
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-17 23:50:27 -0500

    Refactor DropdownCharts to consolidate month and year selection into a single dropdown and improve loading state handling

--- src/presentation/components/dashboard/charts/components/DropdownCharts.tsx
+++ src/presentation/components/dashboard/charts/components/DropdownCharts.tsx
@@ -1,27 +1,32 @@
 import React, { FC } from 'react';
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
-import { deviceWidth } from '../../../../../utils/dimensions';
 import { ThemeColors } from '../../../../theme/ThemeColors';
 import DropdownChart from './DropdownChart';
 
 interface Props {
   colors: ThemeColors;
   isLoading: boolean;
-  monthsAvailable: { label: string, value: string; }[];
-  yearsAvailable: { label: string, value: string; }[];
+  monthYearItems: { label: string, value: string; }[];
   setSelectedMonth: React.Dispatch<React.SetStateAction<string>>;
   setSelectedYear: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const DropdownCharts: FC<Props> = ({ colors, isLoading, monthsAvailable, yearsAvailable, setSelectedMonth, setSelectedYear }) => {
+const DropdownCharts: FC<Props> = ({ colors, isLoading, monthYearItems, setSelectedMonth, setSelectedYear }) => {
   return (
     <>
-      {isLoading ? (
+      {isLoading && !monthYearItems ? (
         <ActivityIndicator size={25} color={colors.mainText} />
       ) : (
-        <View style={[styles.dropdownContainer, { width: deviceWidth * 0.4 }]}>
-          <DropdownChart items={monthsAvailable} placeholder={'Mes'} onChange={setSelectedMonth} />
-          <DropdownChart items={yearsAvailable} placeholder={'Año'} onChange={setSelectedYear} />
+        <View style={styles.dropdownContainer}>
+          <DropdownChart
+            items={monthYearItems}
+            placeholder={'Periodo'}
+            onChange={(value) => {
+              const [month, year] = value.split('-');
+              setSelectedMonth(month);
+              setSelectedYear(year);
+            }}
+          />
         </View>
       )}
     </>
@@ -31,6 +36,7 @@ const DropdownCharts: FC<Props> = ({ colors, isLoading, monthsAvailable, yearsAv
 const styles = StyleSheet.create({
   dropdownContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
