8d36ca07228b64b0cbe888d3d62799859379f539
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-30 23:52:07 -0500

    Refactor SubscriptionButton component to include loading state and onPress handler

--- src/presentation/components/subscriptions/SubscriptionButton/index.tsx
+++ src/presentation/components/subscriptions/SubscriptionButton/index.tsx
@@ -3,19 +3,25 @@ import { StyleSheet } from 'react-native';
 import { ButtonComponent, Footnote } from '../../ui';
 import { ThemeContext } from '../../../theme/ThemeContext';
 
-const SubscriptionButton: FC<{ text: string; }> = ({ text }) => {
+interface Props {
+  loading: boolean;
+  text: string;
+  onPress?: () => void;
+}
+
+const SubscriptionButton: FC<Props> = ({ loading, text, onPress }) => {
   const { colors } = useContext(ThemeContext);
   const currentSubscription = text === 'Suscripción actual';
   const subscriptionBg = currentSubscription ? colors.gray : colors.darkBlue;
   return (
-    <ButtonComponent customStyle={[styles.container, { backgroundColor: subscriptionBg }]} disabled={currentSubscription} loading={false}>
+    <ButtonComponent customStyle={[styles.container, { backgroundColor: subscriptionBg }]} disabled={currentSubscription} loading={loading} onPress={onPress}>
       <Footnote customColor={colors.brandWhite}>{text}</Footnote>
     </ButtonComponent>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { alignSelf: 'flex-start' },
+  container: { alignSelf: 'flex-start', height: 40, width: 135 },
 });
 
 export default SubscriptionButton;
