99a1848fc14fff1eb62faba61ff42a799669d182
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-30 23:53:40 -0500

    Update pricing data type

--- src/presentation/components/subscriptions/SubscriptionPlansList/index.tsx
+++ src/presentation/components/subscriptions/SubscriptionPlansList/index.tsx
@@ -9,30 +9,33 @@ const SubscriptionPlansList: FC = () => {
       <SubscriptionPlastListItem
         level={'3'}
         levelText={'Premium'}
-        price={'$ 20.000'}
+        price={20}
         details={[
           'Alta prioridad en resultado de búsquedas',
           'Subir fotos ilimitadas',
+          'Subir hasta 10 videos promocionales',
           'Registrar más canales de comunicación del lugar (Instagram & WhatsApp).',
         ]}
       />
       <SubscriptionPlastListItem
         level={'2'}
         levelText={'Regular'}
-        price={'$ 15.000'}
+        price={15}
         details={[
           'Media prioridad en resultado de búsquedas',
           'Subir hasta 2 fotos del lugar',
+          'Subir hasta 2 videos promocionales',
           'Registrar más canales de comunicación del lugar (Instagram & WhatsApp).',
         ]}
       />
       <SubscriptionPlastListItem
         level={'1'}
         levelText={'Básico'}
-        price={'$ 0'}
+        price={0}
         details={[
           'Baja prioridad en resultado de búsquedas',
           'Subir 1 foto del lugar',
+          'Subir 1 video promocional',
         ]}
       />
     </ScrollView>
