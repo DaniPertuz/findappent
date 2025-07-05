418198e86e7bdfa588e979642ba178bc84228fe2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-05 01:34:27 -0500

    Add ProductDetailsScreen screen and styles to display product details

---
+++ src/presentation/screens/profile/products/ProductDetailsScreen/index.tsx
@@ -0,0 +1,27 @@
+import { StackScreenProps } from '@react-navigation/stack';
+import React, { FC, useContext } from 'react';
+import { View } from 'react-native';
+import { RootStackParams } from '../../../../navigation/MainNavigator';
+import ProductDetailsHeader from '../../../../components/products/ProductDetailsHeader';
+import ProductItemForm from '../../../../components/products/ProductItemForm';
+import { ThemeContext } from '../../../../theme/ThemeContext';
+import { styles } from './styles';
+import { StatusBarComponent } from '../../../../components/ui';
+
+interface Props extends StackScreenProps<RootStackParams, 'ProductDetailsScreen'> { }
+
+const ProductDetailsScreen: FC<Props> = ({ route }) => {
+  const { colors } = useContext(ThemeContext);
+  const { product } = route.params;
+  return (
+    <>
+      <StatusBarComponent color={colors.brandMainText} theme={'light-content'} />
+      <View style={[styles.mainContainer, { backgroundColor: colors.background }]}>
+        <ProductDetailsHeader product={product} />
+        <ProductItemForm product={product} />
+      </View>
+    </>
+  );
+};
+
+export default ProductDetailsScreen;
---
+++ src/presentation/screens/profile/products/ProductDetailsScreen/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 25,
+  },
+  itemInputsContainer: {
+    marginHorizontal: 15,
+  },
+});
