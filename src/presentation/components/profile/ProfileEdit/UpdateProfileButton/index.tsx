7fd7b3c47072e7f210daf5d5c63eeb3b9c245f3d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-05 01:25:46 -0500

    Add custom component UpdateProfileButton to handle profile update action

---
+++ src/presentation/components/profile/ProfileEdit/UpdateProfileButton/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useContext } from 'react';
+import { Body, ButtonComponent } from '../../../ui';
+import { ThemeContext } from '../../../../theme/ThemeContext';
+
+interface Props {
+  loading: boolean;
+  values: any;
+  onPress: (values: any) => void;
+}
+
+const UpdateProfileButton: FC<Props> = ({ loading, values, onPress }) => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <ButtonComponent disabled={loading} loading={loading} onPress={() => onPress(values)}>
+      <Body customColor={colors.brandWhite}>Guardar cambios</Body>
+    </ButtonComponent>
+  );
+};
+
+export default UpdateProfileButton;
