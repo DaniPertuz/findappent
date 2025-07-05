53fbeb9d553d280604afe971da7fa823ccbc7a2e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-05 01:36:01 -0500

    Update exports

--- src/presentation/screens/profile/index.ts
+++ src/presentation/screens/profile/index.ts
@@ -1,2 +1,4 @@
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as UpdateProfileScreen } from './UpdateProfileScreen';
+export { default as ProductsScreen } from './products/ProductsScreen';
+export { default as ProductDetailsScreen } from './products/ProductDetailsScreen';
