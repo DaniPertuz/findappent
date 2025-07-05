3b5d7ab66549b10d318ec26df0addaa0ec2843f4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-05 01:33:37 -0500

    Add component ProductsList to display products list

---
+++ src/presentation/components/products/ProductsList/index.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { IProduct } from '../../../../core/entities';
+import { deviceHeight } from '../../../../utils/dimensions';
+import ProductItem from '../ProductItem';
+
+interface Props {
+  products: IProduct[];
+}
+
+const ProductsList = ({ products }: Props) => {
+  const scrollViewRef = useRef(null);
+  const contentHeightRef = useRef(0);
+
+  const handleContentSizeChange = (contentHeight: number) => {
+    contentHeightRef.current = contentHeight;
+  };
+
+  return (
+    <ScrollView scrollEnabled={(contentHeightRef.current === deviceHeight - 20)} ref={scrollViewRef} onContentSizeChange={handleContentSizeChange} contentContainerStyle={styles.contentContainer} showsVerticalScrollIndicator={false}>
+      {products.map((product, index) => (
+        <ProductItem key={index} product={product} />
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    marginVertical: 20,
+  },
+});
+
+export default ProductsList;
