7f64c3b04975e736a3917c71730b79f5e4f198dd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-06 17:46:50 -0500

    Refactor ProductItem component to use Pressable instead of TouchableOpacity and simplify image and name handling

--- src/presentation/components/products/ProductItem/index.tsx
+++ src/presentation/components/products/ProductItem/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { Image, Pressable, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParams } from '../../../navigation/MainNavigator';
@@ -16,22 +16,26 @@ interface Props {
 const ProductItem = ({ product }: Props) => {
   const { colors, currentTheme } = useContext(ThemeContext);
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+  const { name, img } = product;
+  const imageUri = img || getIconUrl('', currentTheme, false);
+
+  const handlePress = () => navigation.navigate('ProductDetailsScreen', { product });
 
   return (
     <View style={[styles.container, { backgroundColor: colors.background }]}>
       <View style={styles.productDetailsContainer}>
         <View style={styles.productImageContainer}>
-          <Image source={{ uri: product.img ?? getIconUrl('', currentTheme, false) }} style={styles.productImageStyle} />
+          <Image source={{ uri: imageUri }} style={styles.productImageStyle} />
         </View>
         <View style={styles.productDetailsNameContainer}>
           <Subheadline customColor={colors.mainText}>
-            {product.name}
+            {name}
           </Subheadline>
         </View>
         <View style={styles.productDetailsButtonContainer}>
-          <TouchableOpacity activeOpacity={1.0} style={styles.productDetailsButtonStyle} onPress={() => navigation.navigate('ProductDetailsScreen', { product, newItem: false })}>
+          <Pressable style={styles.productDetailsButtonStyle} onPress={handlePress}>
             <BodySmall customColor={colors.mainText}>Detalles</BodySmall>
-          </TouchableOpacity>
+          </Pressable>
         </View>
       </View>
     </View>
