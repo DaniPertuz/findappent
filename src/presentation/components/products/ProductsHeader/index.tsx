438191577ecd647184c50910612627801d958a10
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-06 17:44:59 -0500

    Refactor ProductsHeader to include ButtonComponent for adding new products and utilize theme colors

--- src/presentation/components/products/ProductsHeader/index.tsx
+++ src/presentation/components/products/ProductsHeader/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { usePlaceStore } from '../../../../store';
 import { RootStackParams } from '../../../navigation/MainNavigator';
-import { BackButton } from '../../ui';
+import { BackButton, ButtonComponent, Caption1 } from '../../ui';
 import ProductsHeaderTitle from '../ProductsHeaderTitle';
+import { ThemeContext } from '../../../theme/ThemeContext';
 
 const ProductsHeader = () => {
+  const { colors } = useContext(ThemeContext);
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+  const place = usePlaceStore(state => state.place);
+
   return (
     <View style={styles.container}>
       <BackButton onPress={() => navigation.goBack()} />
       <ProductsHeaderTitle />
+      <ButtonComponent
+        onPress={() => navigation.navigate('ProductDetailsScreen', {
+          product: {
+            name: '',
+            description: '',
+            price: 0,
+            img: '',
+            categories: [],
+            place: place?._id!,
+            currency: 'COP', // Assuming currency is always COP for now
+          },
+          newItem: true,
+        })}
+      >
+        <Caption1 customColor={colors.mainText}>Agregar</Caption1>
+      </ButtonComponent>
     </View>
   );
 };
