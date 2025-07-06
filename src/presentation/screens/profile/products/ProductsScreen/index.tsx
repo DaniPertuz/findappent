472720de42fa171794873a24c66e3ebe151d18e1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-07-06 17:44:19 -0500

    Refactor ProductsScreen to conditionally render LoadingScreen when no products are available

--- src/presentation/screens/profile/products/ProductsScreen/index.tsx
+++ src/presentation/screens/profile/products/ProductsScreen/index.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
 import ProductsHeader from '../../../../components/products/ProductsHeader';
 import ProductsMainContainer from '../../../../components/products/ProductsMainContainer';
+import ProductsList from '../../../../components/products/ProductsList';
 import { StatusBarComponent } from '../../../../components/ui';
 import { useProductsData } from '../../../../hooks/useProductsData';
-import ProductsList from '../../../../components/products/ProductsList';
+import LoadingScreen from '../../../utils/LoadingScreen';
 
 const ProductsScreen: FC = () => {
   const { colors, currentTheme, products } = useProductsData();
@@ -12,7 +13,10 @@ const ProductsScreen: FC = () => {
       <StatusBarComponent color={colors.background} theme={currentTheme === 'light' ? 'dark-content' : 'light-content'} />
       <ProductsMainContainer>
         <ProductsHeader />
-        <ProductsList products={products} />
+        {products.length === 0
+          ? <LoadingScreen />
+          : <ProductsList products={products} />
+        }
       </ProductsMainContainer>
     </>
   );
