eff589e8236a4b475cc46b69c2d4712e4e892e97
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-11-29 18:27:36 -0500

    feat: enhance ModalHeader component with main image handling and improved layout

--- src/presentation/components/ui/ModalHeader/index.tsx
+++ src/presentation/components/ui/ModalHeader/index.tsx
@@ -1,16 +1,36 @@
 import React, { FC, useContext } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { ProductImage } from '../../../../core/entities';
 import { ModalProps } from '../../../../interfaces/modal-props.interface';
 import ModalHideButton from '../../dashboard/DashboardReviewsModal/ModalHideButton';
 import ModalTitle from '../../dashboard/DashboardReviewsModal/ModalTitle';
+import Footnote from '../Footnote';
 import { ThemeContext } from '../../../theme/ThemeContext';
 
-const ModalHeader: FC<ModalProps> = ({ closeIcon, title, onPress }) => {
+const ModalHeader: FC<ModalProps & {
+  main?: boolean,
+  imgs?: ProductImage[],
+  onPress: () => void,
+  onSetMain?: (uri: string) => void;
+}> = ({ closeIcon, main, title, uri, onPress, onSetMain }) => {
   const { colors } = useContext(ThemeContext);
+
+  const handlePress = () => {
+    if (!main && uri) {
+      onSetMain!(uri);
+    }
+    onPress();
+  };
+
   return (
     <View style={[{ backgroundColor: colors.background }, styles.modalContainer]}>
       <ModalHideButton closeIcon={closeIcon!} onPress={onPress!} />
       {title?.length !== 0 && <ModalTitle title={title!} />}
+      <Pressable onPress={handlePress} disabled={main}>
+        <Footnote customColor={colors.mainText}>
+          {main ? 'Imagen principal' : 'Establecer como principal'}
+        </Footnote>
+      </Pressable>
     </View>
   );
 };
@@ -21,8 +41,10 @@ const styles = StyleSheet.create({
     borderTopEndRadius: 20,
     borderTopStartRadius: 20,
     flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingVertical: 30,
     paddingHorizontal: 20,
+    width: '100%',
   },
 });
 
