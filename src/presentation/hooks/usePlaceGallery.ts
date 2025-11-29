4a48f9825e321cb2863dd212b1d7018892965df4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-11-29 18:20:52 -0500

    feat: implement usePlaceGallery hook for managing place images and videos

---
+++ src/presentation/hooks/usePlaceGallery.ts
@@ -0,0 +1,233 @@
+import { useEffect, useState } from 'react';
+import { ImagePickerResponse } from 'react-native-image-picker';
+import { ImagePickerAdapter } from '../../adapters/ImagePickerAdapter';
+import { useAuthStore, useGalleryStore, usePlaceStore } from '../../store';
+import { deleteCloudinaryPic, handleUpdateCloudinaryPic } from './useCloudinaryOperation';
+
+export const usePlaceGallery = () => {
+  const [profilePicture, setProfilePicture] = useState<ImagePickerResponse>();
+  const [placeImages, setPlaceImages] = useState<string[]>([]);
+  const [placeVideos, setPlaceVideos] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const user = useAuthStore(state => state.authResponse.user);
+  const setUser = useAuthStore(state => state.setUser);
+  const { response, setResponse, imagesToDelete, clearImagesToDelete } = useGalleryStore();
+  const { place, updatePlacePhoto, updateUserPhoto } = usePlaceStore();
+
+  const selectProfilePicture = async () => {
+    const picture = await ImagePickerAdapter.getMultimediaFromLibrary('photo', 1);
+
+    if (!picture || !picture.assets) { return; }
+
+    setProfilePicture(picture);
+  };
+
+  const takeProfilePicture = async () => {
+    const picture = await ImagePickerAdapter.takeMultimedia('photo');
+
+    if (!picture || !picture.assets) { return; }
+
+    setProfilePicture(picture);
+  };
+
+  const addGalleryPic = async () => {
+    setLoading(true);
+    try {
+      const resp = await ImagePickerAdapter.getMultimediaFromLibrary('photo', 1);
+      if (resp.didCancel || !resp.assets || resp.assets.length === 0) {
+        return;
+      }
+
+      const uri = resp.assets![0].uri;
+
+      setPlaceImages([uri!]);
+      setResponse(resp);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addGalleryPics = async (limit: number) => {
+    setLoading(true);
+    try {
+      const resp = await ImagePickerAdapter.getMultimediaFromLibrary('photo', limit);
+
+      if (resp.didCancel || !resp.assets || resp.assets.length === 0) {
+        return;
+      }
+
+      setPlaceImages(prevImages => [
+        ...prevImages,
+        ...(resp.assets?.map(asset => asset.uri).filter((uri): uri is string => !!uri) || []),
+      ]);
+      setResponse(resp);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addPhoto = async () => {
+    setLoading(true);
+    try {
+      const resp = await ImagePickerAdapter.takeMultimedia('photo');
+      if (resp.didCancel || !resp.assets![0].uri) { return; }
+
+      const uri = resp.assets![0].uri;
+
+      setPlaceImages(prevImages => [...prevImages, uri]);
+      setResponse(resp);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const uploadPicture = async () => {
+    setLoading(true);
+    try {
+      if (!profilePicture) {
+        setLoading(false);
+        return;
+      }
+
+      const img = await handleUpdateCloudinaryPic({
+        data: profilePicture,
+        userId: user?._id!,
+        profile: true,
+        deleteExisting: true,
+        existingImg: user?.photo,
+      });
+
+      const [respPlace, respUser] = await Promise.all([
+        updatePlacePhoto(place?._id!, img[0]),
+        updateUserPhoto(user?._id!, img[0]),
+      ]);
+
+      if (!respPlace || !respUser) {
+        setLoading(false);
+        return;
+      }
+      if (respUser) {
+        setUser(respUser);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const uploadPics = async (oldPics: string[]) => {
+    setLoading(true);
+    if (!response) {
+      return;
+    }
+
+    try {
+      if (imagesToDelete && imagesToDelete.length > 0) {
+        await Promise.all(imagesToDelete.map(img => deleteCloudinaryPic(img)));
+      }
+      const remainingPics = oldPics.filter(pic => !imagesToDelete.includes(pic));
+      const updatedPics = await handleUpdateCloudinaryPic({
+        data: response,
+        userId: user?._id!,
+        profile: false,
+        deleteExisting: false,
+        existingImg: remainingPics,
+      });
+
+      clearImagesToDelete();
+
+      return updatedPics;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addGalleryVideo = async () => {
+  setLoading(true);
+  try {
+    const resp = await ImagePickerAdapter.getMultimediaFromLibrary('video', 1);
+    if (resp.didCancel || !resp.assets || resp.assets.length === 0) {
+      return;
+    }
+
+    const uri = resp.assets![0].uri;
+    setPlaceVideos([uri!]);
+    setResponse(resp);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+const addGalleryVideos = async (limit: number) => {
+  setLoading(true);
+  try {
+    const resp = await ImagePickerAdapter.getMultimediaFromLibrary('video', limit);
+    if (resp.didCancel || !resp.assets || resp.assets.length === 0) {
+      return;
+    }
+
+    const uris = resp.assets.map(asset => asset.uri).filter((uri): uri is string => !!uri);
+    setPlaceVideos(prevVideos => [...prevVideos, ...uris]);
+    setResponse(resp);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+const addVideo = async () => {
+  setLoading(true);
+  try {
+    const resp = await ImagePickerAdapter.takeMultimedia('video');
+    if (resp.didCancel || !resp.assets || !resp.assets[0].uri) {
+      return;
+    }
+
+    const uri = resp.assets[0].uri;
+    setPlaceVideos(prevVideos => [...prevVideos, uri]);
+    setResponse(resp);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+  useEffect(() => {
+    setPlaceImages(place?.pics || []);
+    setPlaceVideos(place?.videos || []);
+  }, [place]);
+
+  return {
+    loading,
+    placeImages,
+    placeVideos,
+    profilePicture,
+    response,
+    addGalleryPic,
+    addGalleryPics,
+    addGalleryVideo,
+    addGalleryVideos,
+    addPhoto,
+    addVideo,
+    selectProfilePicture,
+    setPlaceImages,
+    setPlaceVideos,
+    setProfilePicture,
+    takeProfilePicture,
+    uploadPics,
+    uploadPicture,
+  };
+};
